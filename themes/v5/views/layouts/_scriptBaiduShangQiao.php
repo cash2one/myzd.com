b13cf86b43c645db4f54bde7f9207cd9852a6f1b
zhangks <1307716475@qq.com>
2016-01-15 16:55:12 +0800

    删除在线咨询的注释

--- themes/v5/views/layouts/_scriptBaiduShangQiao.php
+++ themes/v5/views/layouts/_scriptBaiduShangQiao.php
@@ -1,4 +1,4 @@
-<!--<div style="display:none;">
+<div style="display:none;">
     <script type="text/javascript"> var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://"); document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Ffec5a293424105cecf25d98670adc148' type='text/javascript'%3E%3C/script%3E"))</script>
-</div>-->
+</div>
 
