8ec2d013e8ae9f5bf3417cb8e16b654fb8a6f6ea
xudongjian <958965736@qq.com>
2016-03-25 11:20:15 +0800

    v5 白内障专题修改

--- themes/v5/views/event/pages/cataract.php
+++ themes/v5/views/event/pages/cataract.php
@@ -26,7 +26,7 @@ Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/eve
                 </div>
                 <div class="text-content mt50">
                     <div class="row">
-                        <div class="col-sm-10 pr0">
+                        <div class="col-sm-10 pr0" style="text-align:justify;">
                             <div class="pull-left"><i class="fa fa-star yellow-star"></i></div>
                             <div><strong>活动介绍</strong>：本次活动合作方为名医主刀和北京熙仁医院眼科，患者可通过名医主刀平台申请获得在熙仁医院免费接受白内障手术治疗名额，手术选择日本进口的豪雅晶体，并有著名白内障专家郝燕生教授示范指导。</div>
                             <div class="clearfix"></div>
@@ -39,11 +39,11 @@ Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/eve
                             <div class="mt10"><strong class="text-red">④</strong> 由名医主刀专业客服与北京熙仁医院专家审核，双方筛选适合白内障单一疾病<strong>前10</strong>名患者。</div>
                             <div class="mt25"><i class="fa fa-star yellow-star"></i><strong>手术时间</strong>：报名成功，审核通过后，即通知手术时间。</div>
                             <div class="mt25"><i class="fa fa-star yellow-star"></i><strong>手术地点</strong>：北京熙仁医院（北京市海淀区学清路 静淑东里10号院）</div>
-                            <div class="mt40 text-red"><strong>注</strong>：本次免费治疗白内障公益活动，只包含手术相关的费用，来往路费不包含在内。</div>
+                            <div class="mt40 text-red"><strong>注</strong>：本次免费治疗白内障公益活动，入院后的一切医疗费用全免（包括住院费、药费、手术费、材料费、检查费），不包含来往路费以及食宿费。</div>
                         </div>
                         <div class="col-sm-2 text-center">
                             <img src="<?php echo $urlResImage ?>event/cataract/doctor.png">
-                            <div class="text12 text-center mt5">图为著名白内障<br/>专家郝燕生主任</div>
+                            <div class="text12 text-center mt5">（图为北京大学第三医院眼科主任医师、著名白内障专家郝燕生教授）</div>
                         </div>
                     </div>
                     <div class="mt25 text18">活动期间有任何疑问，可以微信后台留言或拨打名医主刀热线<strong>400 119 7900</strong>或熙仁医院咨询电话<strong>010-53828088</strong>，我们会在第一时间答复您。</div>
