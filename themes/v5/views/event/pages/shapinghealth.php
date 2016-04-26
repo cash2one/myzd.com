4f5731916d0324d1ce7dbcb03ac58307a6a59177
xudongjian <958965736@qq.com>
2016-04-26 11:41:10 +0800

    v5 "共享名医资源、共铸健康中国"专题

---
+++ themes/v5/views/event/pages/shapinghealth.php
@@ -0,0 +1,48 @@
+<?php
+$urlResImage = Yii::app()->theme->baseUrl . "/images/";
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-shapinghealth.css");
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="header-img">
+            <img class="w100" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146157297300478" />
+        </div>
+    </div>
+</div>
+<div class="container" id="shapinghealth">
+    <div class="main-content mt50">
+        <div class="row">
+            <div class="col-sm-5">
+                <img class="mb10" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146157299324684">
+            </div>
+            <div class="col-sm-7">
+                <div class="content-first">4月20日，“共享名医资源、共铸健康中国；名医公益手术捐赠暨名医公益宣言发布会 ”在人民大会堂如期举行。此次大型公益活动由名医主刀、国内多家公益基金等单位共同发起，力求在“全民健康，全面小康”的总方针下，创新性尝试共享公益资源的新模式。</div>
+            </div>
+        </div>
+        <div class="content-second">中国医药教育协会会长黄正明、名医主刀创始人苏舒、复星医药董事长陈启宇、百度医疗事业部总经理李政、“真格基金”创始人徐小平、名医主刀联合创始人秦鹏和吴勇、安贞医院前副院长白树功、上海德济医院院长郭辉、春晖博爱儿童救助基金会秘书长张志荣等各界公益人士出席活动并现场发言，表达对名医公益事业的支持。此次发布会由原中央电视台主持人，现在同样身体力行于公益事业的郎永淳和钱婧联手主持。</div>
+        <div class="row">
+            <div class="col-sm-6 shapinghealth-img"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146157301948159"></div>
+            <div class="col-sm-6 text-right shapinghealth-img"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146157301933852"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="mt5"><strong>名医主刀创始人苏舒发言表示：</strong></div>
+                <div class="content-third">“名医公益“模式致力于解决我国医疗资源分布不均、“看病难，看病贵”的问题。而这个问题的关键点在于它是需要社会各界参与，共建共享的系统性工程。名医主刀应该发挥互联网企业共享、开放、连接的优势，联合公益组织、医疗专家、医疗机构以及社会力量等各方协力推进。”</div>
+            </div>
+            <div class="col-sm-6">
+                <div class="content-fourth-total">
+                    <div class="mt5"><strong>名医公益模式之共享名医资源</strong></div>
+                    <div class="content-fourth">名医主刀联合全国优秀医务工作者、外科专家为核心志愿者，每名专家每年定向捐赠一定台数的公益手术, 旨在让贫困患者得到公益组织捐款的同时得到名医免费公益手术的机会。需要救助的患者，由慈善公益组织捐赠手术费用，由名医主刀负责对接优质医师，由有公益爱心的医院负责实施手术操作。</div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt30">
+            <div class="col-sm-6">
+                <div class="text18"><strong>名医公益模式之共铸健康中国</strong></div>
+                <div class="content-fifth">除了公益手术捐赠，名医主刀还联合公益组织、社会力量共同开展健康志愿服务，内容涵盖爱心手术、医疗培训、健康讲座、医药捐赠、疾病救助等。这将大力促进中小城市、西部落后地区、农村地区的医疗水平提升，为看不起病的老百姓提供免费高端专家医疗卫生志愿服务，帮助欠发达地区提升整体医疗服务水平和质量。活动还将进一步促成北京、上海等大城市优质医院与地方医疗机构的合作，省级医疗机构与地市县医疗机构的对接，促进优质医疗资源下沉，助力中国医疗卫生事业健康发展，推动分级诊疗、医师多点执业政策全面落地。在发布会的高潮阶段，名医主刀平台上众多有爱心，有能力的国内一线医疗专家发表了自己的“名医公益宣言”和捐赠公益手术的台数；公益组织、企业家代表、公益明星相继探讨了如何借助互联网的优势发挥公益的效能，解患者之难。</div>
+                <div class="content-sixth">“名医公益“的模式，是科学的运用了互联网的优势，吸引大批高端医务工作者作为志愿者加入，增强了当前医疗公益活动的专业性和便捷性，符合互联网科学、民主、分享的价值观，为当代公益事业的发展开创了先河。</div>
+            </div>
+            <div class="col-sm-6 text-right shapinghealth-img"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146157301981569"></div>
+        </div>
+    </div>
+</div>
