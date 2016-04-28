e702b930338e776b100394d80752a729aa62b65b
xudongjian <958965736@qq.com>
2016-04-28 18:36:05 +0800

    v5 百万公益基金修改

--- themes/v5/views/event/pages/millionfund.php
+++ themes/v5/views/event/pages/millionfund.php
@@ -1,49 +1,100 @@
 <?php
 $urlResImage = Yii::app()->theme->baseUrl . "/images/";
-$urlQuickBook = $this->createUrl('booking/quickbook');
-Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-millionfund.css");
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-millionfund.css?v=" . time());
 ?>
-<div class="container-fluid pt30 bg-millonfund">
-    <div class="row">
-        <img src="<?php echo $urlResImage; ?>event/millionfund/header.png" class="img-responsive" alt="" />
+<div class="container-fluid pt30 bg-millonfund" id="millonfund">
+    <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146183901301572" class="img-responsive"/>
+    <div class="text-center main-content">
+        <div class="img-middle"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146183901297115" class="img-responsive"/></div>
+        <div class="mt-130">
+            <div>名医主刀通过整合全国优质医疗资源</div>
+            <div class="mt10">从顶尖专家到闲置床位</div>
+            <div class="mt10">并借助资本力量和品牌影响力</div>
+            <div class="mt10">为有手术需求的患者提供公益支持和帮助</div>
+        </div>
+        <div class="red-heading mt20"><strong class="mr50">100万公益基金</strong><strong>100位手术患者</strong></div>
+        <div class="text21 mt20"><strong>我们向手术有困难的每位患者提供1万元人民币的专家手术费资助</strong></div>
+        <div class="text21"><strong>如情况更为特殊，我们会额外增资</strong></div>
+
     </div>
+    <div class="row mt30">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-1">
+                </div>
+                <div class="col-md-5">
+                    <div class="apply"><span class="number-mark">1</span>申请方式一：</div>
+                    <div class="mt10">拨打服务热线400-6277-120并告知名医助手申请资助。</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="apply"><span class="number-mark">2</span>申请方式二：</div>
+                    <div class="mt10">直接在线（网站/App/微信）预约专家，客服回访确认时提出资助需求。</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center mt30 color-e50011 text16">*名医助手将对您的病例和申请进行审核，如未通过，我们也将全力帮助您安排到顶尖专家尽快进行手术。</div>
+    <div class="row"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/14618390128216" class="img-responsive"/></div>
+
 </div>
 <div class="container-fluid bg-millonfund">
     <div class="row">
         <div class="container">
             <div class="row">
-                <div id="millonfund-content" class="text-center">
-                    <div class="header">“名医主义”--名医主刀旗下医疗公益项目</div>
-                    <div class="mt16">通过整合全国优质医疗资源</div>
-                    <div class="mt14">借助资本力量和品牌影响力</div>
-                    <div class="mt14">为有手术需求的患者提供公益支持和帮助</div>
-
-                    <div class="mt46">如果你还在为找不到专家而发愁</div>
-                    <div class="mt14">如果你还在为等不到床位而苦恼</div>
-                    <div class="mt14">如果你还在为手术费用而烦忧</div>
-                    <div class="mt14">那么就来<span class="color-21a59c">“名医主义”</span></div>
-
-                    <div class="mt46">“名医主义”将为手术患者提供:</div>
-                    <div class="clearfix mt25"><span class="tag">专家资源</span><span class="tag ml44">床位资源</span><span class="tag ml44">保险服务</span><span class="tag ml44 pl7 pr6">100万公益金</span></div>
-
-                    <div class="mt30">百万基金将用于</div>
-                    <div class="mt14">为患者提供手术治疗的专家费用</div>
-                    <div class="mt14">每位患者可获得<span class="ten-thousand inline-block">“一万”</span>元专家手术费的资助</div>
-                    <div class="mt14">若患者情况特殊还可向百万基金申请额外增资</div>
-
-                    <div class="mt46">
-                        <img src="<?php echo $urlResImage; ?>event/millionfund/step-01.png" alt="" />
-                    </div>
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-4" style="margin-left:20px;">
-                        <div class="text-left"><div class="mt32">1.网站</div><div class="mt14">2.APP</div><div class="mt14">3.微信公众号(在搜索栏中输入“名医主刀”即可)</div><div class="mt14">4.服务热线 <span class="color-21a59c strong">400-6277-120</span></div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4"></div>
-                    <div class="col-sm-3 text-left" style="margin-left:-20px;"><div class="mt32">"名医主义"评估委员会将</div><div class="mt14">对您提交的病例和申请进行审核</div><div class="mt14">审核通过后客服会立即联系您</div><div class="mt14">为您尽快安排手术</div></div>
+                <div class="text-center"><div class="table-top-yellow-mark">目前获得资助的患者：</div></div>
+                <div>
+                    <table class="table millonfund-table">
+                        <thead>
+                            <tr class="bg-fad5a9"><th>申请日期</th><th>患者姓名</th><th class="w22">疾病诊断</th><th>对接专家</th><th>所在医院</th><th>手术日期</th><th>资助费用</th></tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>2016.01.11</td><td>陈*聪</td><td>腺样体肥大</td><td>刘大波</td><td>广州市妇女儿童医疗中心</td><td>2016.01.13</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.13</td><td>欧*秋</td><td>鼻咽癌放疗后</td><td>史剑波</td><td>广州中山大学附属第一医院</td><td>2016.01.15</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.14</td><td>戴*丽</td><td>腰椎间盘突出症</td><td>顾昕</td><td>上海第十人民医院</td><td>2016.01.22</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.19</td><td>潘*枝</td><td>双股骨头坏死</td><td>史占军</td><td>广州南方医院</td><td>2016.01.20</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.20</td><td>张*鸠</td><td>左中耳炎</td><td>张志钢</td><td>广州中山大学附属第二医院</td><td>2016.01.29</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.20</td><td>崔*良</td><td>胆管癌</td><td>张永杰</td><td>上海东方肝胆外科医院</td><td>2016.01.28</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.23</td><td>渝*平</td><td>垂体腺瘤卒中</td><td>周涛</td><td>北京301医院</td><td>2016.01.23</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.26</td><td>戴*培</td><td>食管癌</td><td>谭黎杰</td><td>上海中山医院</td><td>2016.02.01</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.28</td><td>马*荣</td><td>十二指肠肿物</td><td>别平</td><td>重庆西南医院</td><td>2016.01.31</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.01.29</td><td>平**扎</td><td>斜视</td><td>乔彤</td><td>上海儿童医院</td><td>2016.02.01</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.02.19</td><td>熊*根</td><td>左侧复发性腹股沟疝</td><td>顾岩</td><td>上海第九人民医院</td><td>2016.02.19</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.02.23</td><td>邓*胜</td><td>视网膜脱离</td><td>江睿</td><td>上海五官科医院</td><td>2016.02.26</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.03.15</td><td>付*</td><td><div title="左膝前叉韧带断裂左膝半月板损伤，痛风，滑膜炎">左膝前叉韧带断裂左膝半月板损伤...</div></td><td>陈世益</td><td>上海华山医院</td><td>2016.03.24</td><td>费用10000元</td>
+                            </tr>
+                            <tr>
+                                <td>2016.03.28</td><td>曹*武</td><td>左侧气胸</td><td>刘彦国</td><td>北京大学人民医院</td><td>2016.03.28</td><td>费用10000元</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+                <div class="text-center color-19aea5 text16 mb100">名额更新中...</div>
             </div>
         </div>
     </div>
-</div>
 </div>
