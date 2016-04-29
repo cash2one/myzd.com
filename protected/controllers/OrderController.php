514440e7f2c3727fc9600d477b8c191093928de2
zhangks <1307716475@qq.com>
2016-04-29 10:31:17 +0800

    完成义诊定金优化

--- protected/controllers/OrderController.php
+++ protected/controllers/OrderController.php
@@ -41,20 +41,24 @@ class OrderController extends WebsiteController {
 
     //完成义诊定金支付并修改订单状态
     public function actionPayDeposit($refno) {
-        $model = SalesOrder::model()->getByAttributes(array('ref_no' => $refno));
-        $booking = Booking::model()->getByRefNo($model->bk_ref_no);
-        if ($booking->booking_service_id == BookingServiceConfig::BOOKING_SERVICE_FREE_LIINIC) {
-            $adminBooking = AdminBooking::model()->getByAttributes(array('ref_no' => $model->bk_ref_no));
-            $model->is_paid = SalesOrder::ORDER_PAIDED;
-            $model->date_closed = new CDbExpression('NOW()');
-            $booking->bk_status = StatCode::BK_STATUS_PROCESSING;
-            $adminBooking->booking_status = StatCode::BK_STATUS_PROCESSING;
-            $adminBooking->work_schedule = StatCode::BK_STATUS_PROCESSING;
-            if ($booking->save() && $adminBooking->save() && $model->save()) {
+        $model = SalesOrder::model()->getByAttributes(array('ref_no' => $refno, 'order_type' => SalesOrder::ORDER_TYPE_DEPOSIT));
+        if (isset($model)) {
+            $booking = Booking::model()->getByRefNo($model->bk_ref_no);
+            if ($booking->booking_service_id == BookingServiceConfig::BOOKING_SERVICE_FREE_LIINIC) {
+                $adminBooking = AdminBooking::model()->getByAttributes(array('ref_no' => $model->bk_ref_no));
+                $model->is_paid = SalesOrder::ORDER_PAIDED;
+                $model->date_closed = new CDbExpression('NOW()');
+                $booking->bk_status = StatCode::BK_STATUS_PROCESSING;
+                $adminBooking->booking_status = StatCode::BK_STATUS_PROCESSING;
+                $adminBooking->work_schedule = StatCode::BK_STATUS_PROCESSING;
+                if ($booking->save() && $adminBooking->save() && $model->save()) {
+                    $this->redirect(array('booking/userBooking', 'id' => $booking->getId()));
+                }
+            } else {
                 $this->redirect(array('booking/userBooking', 'id' => $booking->getId()));
             }
         } else {
-            $this->redirect(array('booking/userBooking', 'id' => $booking->getId()));
+            throw new CException('该订单-' . $refno . ' 不存在');
         }
     }
 
