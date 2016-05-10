fd314ecdfbfe84c55cec3141876ecaee3efcdcfe
Administrator <Administrator@USER-20160315ZV>
2016-05-10 16:57:26 +0800

    改版支付页面及功能

--- themes/v5/views/order/viewOne.php
+++ themes/v5/views/order/viewOne.php
@@ -53,7 +53,7 @@ if (isClientWeixin()) {
 <div class="bg-green">
     <div class="container">
         <div class="user-crumbs">
-            <a href="<?php echo Yii::app()->homeUrl; ?>">首页88888888</a>
+            <a href="<?php echo Yii::app()->homeUrl; ?>">首页999999</a>
             >
             <a href="<?php echo $urlBookingList; ?>">个人中心</a>
             >
