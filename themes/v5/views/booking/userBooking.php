948bbd1e2360b8834ab5c082469f8c984585c6d0
Administrator <Administrator@USER-20160315ZV>
2016-05-13 19:17:40 +0800

    改版支付页面及功能

--- themes/v5/views/booking/userBooking.php
+++ themes/v5/views/booking/userBooking.php
@@ -162,7 +162,8 @@ $urlResImage = Yii::app()->theme->baseUrl . "/images/";
                             <div class="pl20 mt30">
                                 <div class="bookingInfo">患者姓名：<?php echo $booking->patientName; ?></div>
                                 <div class="bookingInfo">联系方式：<?php echo $booking->mobile; ?></div>
-                                <?php if(!empty($paid_count)):?><div class="bookingInfo"> 支付费用：已支付预约金<?php echo $paid_count; ?>元</div><?php endif;?>
+                                <?php if(!empty($paid_count['deposit_count'])):?><div class="bookingInfo">  支付费用：已支付预约金<?php echo $paid_count['deposit_count']; ?>元</div><?php endif;?>
+                                 <?php if(!empty($paid_count['paid_count'])):?><div class="bookingInfo"> 已支付预约金<?php echo $paid_count['paid_count']; ?>元</div><?php endif;?>
                             </div>
                         </div>
                         <div class="col-md-4">
