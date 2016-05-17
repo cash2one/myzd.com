850e319f22cbdccb63ef6224b0a67a14c8e7c796
xudongjian <958965736@qq.com>
2016-05-17 10:42:38 +0800

    个人中心修改密码界面

--- themes/v5/views/user/changePassword.php
+++ themes/v5/views/user/changePassword.php
@@ -22,9 +22,9 @@ $urlBookingList = $this->createUrl('booking/list');
         </div>
         <div class="col-sm-9">
             <div>
-                <img class="img-responsive" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146010447580821">
+                <img class="img-responsive" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146277800063795">
             </div>
-            <div class="bookinglist border-green mt10">
+            <div class="bookinglist border-gray mt10">
                 <div class="mt50">
                     <?php $this->renderPartial('_formPassword', array('model' => $model)); ?>
                 </div>
