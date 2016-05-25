f52162c94b5744c461aefa3c101899d8e4f3be00
xudongjian <958965736@qq.com>
2016-05-25 18:29:22 +0800

    新专题页面

---
+++ themes/v5/views/event/pages/redcommonweal.php
@@ -0,0 +1,74 @@
+<?php
+$urlResImage = Yii::app()->theme->baseUrl . "/images/";
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-main.min.css?v=" . time());
+?>
+<section id="redcommonweal">
+    <div class="redcommonweal-bgcolor">
+        <div class="redcommonweal-picture-move"><img class="redcommonweal-picture-height" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146414797634589" alt=""/></div>
+    </div>
+    <div class="container">
+        <div class="mt20">
+            <div class="row">
+                <div class="text-center">
+                    <div class="col-sm-6">
+                        <div class="redcommonweal-text-litle mt10" >
+                            近日，中国最大的移动医疗手术平台——“名医主刀”与延安大学附属医院成功签约，双方将以延安大学附属医院为区域手术基地，
+                            合作共建延安市第一家互联网手术中心。此举也标志着名医主刀“红色公益之旅”大型活动正式启动。名医主刀携手权威眼科专家朱
+                            思泉教授为延安23位白内障患者展开公益手术，帮助他们恢复视力。同时，名医主刀和复星公益基金联合为每位患者捐赠1000元手术费，
+                            让患者切实感受到公益力量的温暖。
+                        </div>
+                        <div class="mt20"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146414797581681" alt=""/></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="redcommonweal-text-litle mt10">
+                            “名医主刀”创始人兼CEO苏舒介绍，以延安为代表的老区曾为中国的革命事业做出过巨大的贡献。但当前老区优质医疗资源相对匮乏，
+                            当地人民看好病的成本高。针对这一现状，名医主刀将以互联网手术中心为载体，采用“名医铸刀”模式，一方面提升地方医疗服务水准，
+                            另一方面让延安当地患者享受到权威专家的手术治疗，患者可以大病不出市，切实享受到分级诊疗的红利。
+                        </div>
+                        <div class="mt40"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146414797606044" alt=""/></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="mt20">
+            <div class="row">
+                <div class="text-center">
+                    <div class="col-sm-6 mb50">
+                        <div class="redcommonweal-text-litle mt20">
+                            据了解，由于优质医疗资源分布的不均衡，医疗资源紧张所导致的看病难、看病贵等问题十分突出。因而，实施以分级诊疗制度为核心的医疗
+                            改革就显得尤为迫切。以做手术这领域为突破口，“名医主刀”通过优化名医资源配置来盘活整个分级诊疗链条，让普通患者也能享受到名医的医疗服务。                                
+                        </div>
+                        <div class="mt20"><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146414797615289" alt=""/></div>
+                    </div>
+                    <div class="col-sm-6 mb50">
+                        <div class="redcommonweal-text-litle mt20">
+                            专家指出，“名医主刀”的模式真正促成了优质医疗资源下沉，让北京、上海等大城市优质医院与地方医疗机构合作，省级医疗机构与地市县医疗机构对接，实现了优
+                            质医疗资源的再分配，对推动分级诊疗、医师多点执业政策全面落地将发挥重要的助推作用。
+                        </div>
+                        <div class="mt20 "><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146414797623939" alt=""/></div>
+                    </div>
+                </div>
+            </div>
+            <div class="redcommonweal-text-litle mb50">
+                在“名医主刀”平台上，患者只需注册后填写资料，平台会结合患者的病情做最优的专家匹配。通过匹配专家的专业领域和时间，进行初步沟通，达成一致意见以后，
+                平台帮助联络空置床位和手术台进行手术。如此一来，一台手术的等待时间被大大缩短，从咨询到预约只需要2-7天。据统计，自2014年10月上线以来，平台已汇集2
+                万余名三甲医院副主任级别及以上的医生，促成了近万台手术，收效十分显著。<br>
+                <div>
+                    据悉，名医主刀“红色公益之旅”还将走进更多的老区，提供医疗培训、健康讲座、医药捐赠、疾病救助等健康公益服务，帮助老区提升整体医疗服务水平和质量，
+                    让更多老区人民从“互联网+医疗”以及“分级诊疗制度”中受益，践行品牌的社会责任和公益担当。</div>
+            </div> 
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="redcommonweal-text-litle-last">
+                        <span>您可以将手术需求告诉我们，如果未指定专家，名医助手将会为您匹配最适合您病情的主刀专家。</span>
+                    </div>
+                </div>
+                <div class="col-sm-3 text-center">
+                    <button type="button" class="btn redcommonweal-btn-green" data-toggle="modal" data-target="#qucikbookingModal" >一键预约</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
