74f61b475e7a0271372ece79c0e10a1f0d3dd0a3
Administrator <Administrator@USER-20160315ZV>
2016-05-26 11:51:55 +0800

    更改 搜索找医院 二级科室 bug

--- protected/apiservices/ApiViewDiseaseCategory.php
+++ protected/apiservices/ApiViewDiseaseCategory.php
@@ -1,12 +1,15 @@
 <?php
 
 class ApiViewDiseaseCategory extends EApiViewService {
+
     public function __construct() {
         parent::__construct();
     }
 
     protected function loadData() {
+//        $this->loadCity();
         $this->loadDiseaseCategory();
+        //查询医生city_id 城市
     }
 
     protected function createOutput() {
@@ -19,9 +22,18 @@ class ApiViewDiseaseCategory extends EApiViewService {
             );
         }
     }
-    public function loadDiseaseCategory(){
+
+    public function loadCity($values = array()) {
+        $cri = new CDbCriteria();
+        $cri->addInCondition('id', $values);
+        $res = RegionCity::model()->findAll($cri);
+    }
+
+    public function loadDiseaseCategory() {
         $disMgr = new DiseaseManager();
-        $models = $disMgr->loadDiseaseCategoryListV7();
+        // $models = $disMgr->loadDiseaseCategoryListV7();
+        $disease = Yii::app()->request->getParam('disease');
+        $models = $disMgr->loadHospitalList($disease);
         $navList = array();
         foreach ($models as $model) {
             $data = new stdClass();
@@ -36,14 +48,14 @@ class ApiViewDiseaseCategory extends EApiViewService {
             if (isset($navList[$data->id])) {
                 $navList[$data->id]->subCat[] = $subGroup;
             } else {
-
                 $navList[$data->id] = $data;
                 $navList[$data->id]->subCat[] = $subGroup;
             }
         }
         $this->setDiseaseCategory(array_values($navList));
     }
-    private function setDiseaseCategory($data){
+
+    private function setDiseaseCategory($data) {
         $this->results = $data;
     }
 
