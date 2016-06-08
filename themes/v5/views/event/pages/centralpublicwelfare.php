27b966e4aba4a7c992174a127a834ede326f948a
xudongjian <958965736@qq.com>
2016-06-08 17:30:18 +0800

    两个专题

---
+++ themes/v5/views/event/pages/centralpublicwelfare.php
@@ -0,0 +1,131 @@
+<?php
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-main.min.css?v=" . time());
+?>
+<section id="centralpublicwelfare">
+    <div class="Centralpublicwelfare-bgcolor">
+        <div class="container"><img class="header-img" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146535429302443" alt=""/></div>
+    </div>
+    <div class="container mt30">
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146535428250189" alt=""/>
+            </div>
+            <div class="col-sm-8">
+                <div class="Centralpublicwelfare-text-litle">
+                    近日，名医主刀互联网手术中心-延安大学附属医院，迎来了北京的权威胸外科专家刘彦国教授和北京的肝胆外科专家李志伟教授。
+                    两位教授不辞劳苦，为期盼已久的两位老区患者实施了肺癌和肝癌手术，手术圆满成功。这标志着互联网手术中心的真正落地和
+                    名医主刀的红色公益之旅更加深入了老区人民。</div>
+                <div class="Centralpublicwelfare-text-litle">
+                    名医主刀继成功与延安大学附属医院签约共建互联网手术中心，开启“红色公益之旅”大型活动以来，中国最大的移动医疗手术平台——“名医主刀”再次
+                    为延安患者送去福音，互联网手术中心增设肝胆外科和胸外科两大科室的公益手术，让更多老区患者分享分级诊疗医改红利。
+                </div>
+            </div>
+        </div>
+        <div class="Centralpublicwelfare-text-litle-black mt50"> 
+            <div>
+                据悉，在此前名医主刀“红色公益之旅”延安站的首发活动中，名医主刀携手权威眼科专家朱思泉教授
+                为20名白内障患者提供了手术医疗援助，取得了良好的社会反响，受到了当地群众的热烈欢迎。
+            </div>
+            <div>
+                响应群众呼声，名医主刀此次在白内障手术的基础上，新增了肝胆外科和胸外科两大公益手术科室。来自北京的权威胸外科专家刘彦国教授为1名患者实施了肺癌手
+                术，在术后探访了患者并送去了公益福利。
+            </div>
+            <div class="Centralpublicwelfare-picture-right mt10 mr5">
+                <img  src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146535428512512" alt=""/>
+            </div>
+            <div>
+                当来自北京大学人民医院的刘彦国教授代表名医主刀联合复星公益基金为老区患者捐赠1000元手术费的时候，老区患者的家属开心的说：“真是太温暖，太感谢了！拍个合影留个念吧！”
+            </div>
+            <div>
+                来自北京的李志伟教授这次为患者施行的是一例复杂的肝癌手术，为了能在术前给患者制订一个最佳的手术方案，李志伟教授和延安当地医生一道，首次使用了名
+                医主刀平台联合上海EDDA科技为全国各地外科医生开放的“精准手术规划平台”，该平台能在术前为患者进行精准的手术模拟切除三维设计。就是在这例患者中，
+                专家们利用该技术意外地发现了之前常规CT检查没有发现的肝脏血管变异情况，经过延安、北京两地医学专家和EDDA技术团队的共同努力，最终设计出了一个符合
+                患者病情的个性化手术切除方案，使手术得以快速安全地完成。手术专家们对精准手术规划技术的“精”和“准”给予了高度评价。
+            </div>
+            <div>
+                李志伟教授这次不仅为延安当地患者实施了免费公益手术，还特地为当地医生进行了一场专业知识讲座。李教授这样通过术前共同分析病例、EDDA科技的远程精准
+                手术模拟规划、术中手把手指导操作、术后分享交流等形式的定点帮扶，不仅切实提高了当地医生的专业水平，也让名医主刀平台的“名医铸刀”计划全面落地。
+                李志伟教授告诉我们：“名医铸刀这个方式为医生定点帮扶，提高专业水平提供了便捷而有力的平台。我很乐意将我所学所有的跟老区的医生们分享，也很愿意以
+                自己所学通过名医铸刀这个平台帮助到更多的医生。在惠及患者之余，名医主刀互联网手术中心又增设了更多的手术科目，对入驻平台的医生而言，将带来更多与
+                需要手术服务的患者对接的机会，我们的空闲时间充分利用起来，实现专业价值与个人价值的最大化，极大地调动了医生的积极性，真正实现医患双方的共赢。 ”
+            </div>
+        </div>
+        <div class="mt50">
+            <div class="Centralpublicwelfare-picture-left" >
+                <img class="mt10" src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146535428575870" alt=""/>
+            </div>
+            <div class="Centralpublicwelfare-text-litle-black">
+                <div >
+                    刚刚结束手术的刘彦国教授接受了记者的采访，他说：“名医主刀与延安大学附属医院做的这个互联网手术中心真是太好了。如果不是这个平台，我们也很难
+                    达成来到延安老区，奉献我们的技术和爱心啊！名医主刀以互联网手术中心为载体，采用“名医铸刀”模式，有力提升地方医疗服务水准的同时，更使当地患
+                    者足不出市便能享受到权威专家的手术治疗。如此一来，以分级诊疗为代表的医改福利对他们而言是真切而实在的，大受欢迎也就不言而喻了。”
+                </div>
+                <div >
+                    截至目前，名医主刀互联网手术中心由单纯的白内障手术扩展到白内障、肝胆外科以及胸外科等多科室，标志着名医主刀“互联网手术中心”战略深深扎根在
+                    了延安。一方面，更多来自全国知名三甲医院副主任级别及以上的权威医生将聚集在“名医主刀”平台上，科室门类、名医阵容均将获得大幅扩展和提升。另
+                    一方面通过优化名医资源配置，名医主刀更有效地盘活整个分级诊疗体系。在该模式下，北京、上海等大城市优质医院实现了与地方医疗机构的合作、省级医
+                    疗机构实现了与地市县医疗机构的对接，实现了优质医疗资源的下沉和再分配，有力地推动了分级诊疗制度和医师多点执业政策的全面落地实施，让“互联网
+                    +医疗”以及分级诊疗制度的红利惠及更多普通群众。
+                </div>
+            </div>
+        </div>
+        <div class="Centralpublicwelfare-text mt50">
+            李志伟教授正借助EDDA科技的精准手术规划平台，现场远程模拟手术方案
+        </div>
+        <div class="row">
+            <div class="col-sm-9 mt20">
+                <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146536841742318" alt=""/>
+            </div>
+            <div class="col-sm-3 mt20 pl0">
+                <div class="Centralpublicwelfare-last">
+                    据不完全统计，名医主刀平台根据患者实际病情、专家的专业领域和时间进行最优
+                    匹配的模式，大大节省了患者的手术等待时间和综合成本。自2014年10月上线以来已促成手术近万台，获得了医患双方的广泛欢迎和好评。
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mb50">
+        <div class="row">
+            <div class="col-sm-6 col-lg-4 mt50">
+                <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '85')); ?>">
+                    <div class="border-gray expert-list">
+                        <div><img class="img100" src="http://dr.static.mingyizhudao.com/4F10705C31AE305A07B256283A8B5CDB" alt=""></div>
+                        <div class="pull-right contracted-img"></div><div class="clearfix"></div>
+                        <div class="pull-right mingyiyizhen-img"></div><div class="clearfix"></div>
+                        <div class="text-center"><span class="strong ml20">朱思泉</span><span class="text-center">&nbsp;主任医师&nbsp;教授</span></div>
+                        <div class="text-center mt5 hpDeptName">白内障中心</div>
+                        <div class="text-center mt5">北京同仁医院</div>
+                        <div class="mt10 docdesc"><span class="strong">擅长：</span>白内障、近视、老花眼的治疗。</div>                      
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-lg-4 mt50">
+                <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '2939')); ?>">
+                    <div class="border-gray expert-list">
+                        <div><img class="img100" src="http://dr.static.mingyizhudao.com/960B25FD0970166787774B129807920A" alt=""></div>
+                        <div class="pull-right contracted-img"></div><div class="clearfix"></div>
+                        <div class="pull-right mingyiyizhen-img"></div><div class="clearfix"></div>
+                        <div class="text-center"><span class="strong ml20">刘彦国</span><span class="text-center">&nbsp;主任医师&nbsp;</span></div>
+                        <div class="text-center mt5 hpDeptName">胸外科</div>
+                        <div class="text-center mt5">北大人民医院</div>
+                        <div class="mt10 docdesc"><span class="strong">擅长：</span>手汗症、肺癌、胸腺瘤、肺大泡。</div>                      
+                    </div>
+                </a>
+            </div>
+            <div class="col-sm-6 col-lg-4 mt50">
+                <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '3017')); ?>">
+                    <div class="border-gray expert-list">
+                        <div><img class="img100" src="http://dr.static.mingyizhudao.com/0048D8B24CC66ED65BEA0576D276A93B" alt=""></div>
+                        <div class="pull-right contracted-img"></div><div class="clearfix"></div>
+                        <div class="pull-right mingyiyizhen-img"></div><div class="clearfix"></div>
+                        <div class="text-center"><span class="strong ml20">李志伟</span><span class="text-center">&nbsp;主任医师&nbsp;教授</span></div>
+                        <div class="text-center mt5 hpDeptName">北京302医院</div>
+                        <div class="text-center mt5">肝脏外科</div>
+                        <div class="mt10 docdesc"><span class="strong">擅长：</span>行开腹肝硬化门静脉高压症脾切除贲门周围血管离断术3000余例，腹腔镜下微创脾切除贲周围血管离断术100余例，为全国开腹及腹腔镜门静脉高压症手术最多的医生及单位之一，另外擅长腹腔镜下肝脏肿瘤切除及腹腔镜...</div>                      
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="text-right mt5"><span class="learn-more-expert"><a href="<?php echo $this->createUrl('site/page', array('view' => 'mygy')); ?>" target="_blank">查看更多公益手术专家&gt;&gt;</a></span></div>
+    </div>
+</section>
