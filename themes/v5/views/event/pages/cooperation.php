9508aec913df1cf77662ff2e36c5a689a7d74547
xudongjian <958965736@qq.com>
2016-07-14 17:21:31 +0800

    新专题

---
+++ themes/v5/views/event/pages/cooperation.php
@@ -0,0 +1,67 @@
+<?php
+Yii::app()->clientScript->registerCssFile(Yii::app()->theme->baseUrl . "/css/event-main.min.css?v=" . time());
+?>
+<div id="cooperation">
+<div>
+            <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/14683119356409">
+        </div> 
+        <div class="container  mt20">
+            <div class="cooperation-padding-85">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="border-488">
+                            <div class="cooperation-text-16 mt30">
+                                近日，名医主刀创始人兼CEO苏舒，联合创始人兼首席技术官秦鹏以及首席财务官叶菲等一行前往美国纽约，与美国凯瑟琳癌症中心完成了股权合作，标志着双方的合作迈入实质性的深度阶段。
+                            </div>
+                            <div class="cooperation-text-16 mt30 cooperation-mb-115">此前，在名医主刀B轮融资中，美国凯瑟琳癌症中心的核心团队成员不远万里为投资而来，且最终选定名医主刀成为美国凯瑟琳癌症中心在中国唯一的投资项目，可见他们真诚而又郑重的合作意向。</div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div><img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/14683101499410"> </div>
+                        <div class="cooperation-text-12 mt10">美国凯瑟琳癌症中心第五代传人Mr. Grant Kettering、名医主刀创始人兼CEO苏舒</div>
+                    </div>
+                </div>
+                <div class="mt30">
+                <div class="cooperation-text-18" style=" background-color:#fff;z-index:99;display: inline-block;">
+                    <div class="cooperation-border-fff"><strong>合力打造全球化肿瘤服务平台</strong></div>
+                </div>
+                <div class="cooperation-border-color" style="margin-top:-25px;"></div>
+                </div>
+                <div class="row mt20">
+                    <div class="col-lg-6">
+                        <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146831476742078">
+                        <div class="cooperation-text-12 mt10">名医主刀联合创始人兼首席技术馆秦鹏、首席财务官叶菲、创始人兼CEO苏舒</div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="border-488">
+                            <div class="cooperation-text-14 mt20">
+                                名医主刀商务团队美国之行，更是有备之行，诚意之行。这一点在双方达成的合作备忘中可见一斑。</div>
+                            <div class="cooperation-text-14 mt30">名医主刀与美国凯瑟琳癌症中心将合力打造一个专业的全球化肿瘤服务平台，美国凯瑟琳癌症中心的专家通过平台，加入到我们中国患者的远程咨询，转诊会诊的流程中，快速直接地将他们的经验分享给中国的患者。</div>
+                            <div class="cooperation-text-14 mt30">这样，中国的患者再也不用支付难以负担的昂贵的看病费用，而是通过名医主刀的国际就医端口，直接预约，足不出国就能“看好病”。</div>
+                            <div class="cooperation-text-14 mt30 cooperation-mb80">如果说，名医主刀移动医疗手术平台此前在中国范围内，解决了“好看病，看好病”的医疗痛点的话，与美国凯瑟琳癌症中心的深度合作无疑是将“好看病，看好病”的领域再次扩张，实现了在全球范围内的“好看病，看好病”。</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt30">
+                <div class="cooperation-text-18" style=" background-color:#fff;z-index:99;display: inline-block;">
+                    <div class="cooperation-border-fff"><strong>同心发力互联网+手术新篇章</strong></div>
+                </div>
+                <div class="cooperation-border-color" style="margin-top:-25px;"></div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 cooperation-text-14 mt20">一百多年来，美国凯瑟琳癌症中心一直专注于各类癌症的研究、诊断与治疗，积累了雄厚卓著的实力，无论是知名医生的团队阵容还是手术方法的突破创新都领跑全美，每年所实施的各类癌症手术数量更是位居美国首位。正因如此，美国凯瑟琳癌症中心成为美国国家总统以及国家领导人的御用医疗机构。</div>
+                    <div class="col-lg-12 mt30"style="text-align: center;">
+                        <img src="http://7xsq2z.com2.z0.glb.qiniucdn.com/146831692042525">
+                        <div class="cooperation-text-12 mt10">名医主刀创始人兼CEO 苏舒苏舒、凯瑟琳癌症中心副总裁兼国际合作部总负责人Dr. Brennan、凯瑟琳癌症中心国际合作部行政主管Dr. Noreen</div>
+                    </div>
+                </div>
+                <div class="mt30 mb50">
+                        <div class="cooperation-font-color">
+                            名医主刀则通过优化名医资源配置的模式，根据患者实际病情、专家的专业领域和时间进行最优匹配的模式，不仅促进了优质医疗资源的下沉，让更多普通患者从医改受益，更实现了医患双方的共赢。对患者而言，通过名医主刀不仅能享受全国知名医生的手术服务，而且手术等待时间和综合成本也大大缩减。
+                        </div>
+                    <div class="cooperation-font-color">美国凯瑟琳癌症中心与名医主刀的强强联合，意味着名医主刀已经开启国际化拓展的重要一步。对于美国凯瑟琳癌症中心来讲，也标志着他们正式进军中国市场。全球癌症领域最先进的医疗技术，加上互联网优化的资源整合，谱写了新一轮互联网+手术的新篇章。</div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
