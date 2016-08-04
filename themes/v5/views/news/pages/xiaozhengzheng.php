112b19b35cb9d721d61236448af430bbb3a77661
xudongjian <958965736@qq.com>
2016-08-04 11:33:44 +0800

    患者故事

---
+++ themes/v5/views/news/pages/xiaozhengzheng.php
@@ -0,0 +1,102 @@
+<?php ?>
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="">
+            <div class="col-sm-9 news-content pt30">
+                <div style="text-align:center;">
+                    <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>小政政做手术准备当男孩王艳和名医主刀联手助力</strong></span>
+                </div>
+                <p>
+                    <br />
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp; 昨日，让广大北京市民和媒体“牵肠挂肚”的双性儿——小政政在中国医学科学院整形外科医院成功进行了尿道下裂手术，为变身为男孩迈出了坚实的一步。其实，在小政政手术成功的背后，离不开社会各界的共同关注。据悉，名医主刀在得知小政政病情后，第一时间为孩子安排好了专家、医院等资源，同时协调了其公益大使“晴格格”王艳的捐助工作。&nbsp;</span>
+                </p>
+                <p>
+                    <img src="http://static.mingyizhudao.com/147010941042228">
+                </p>
+                <p>
+                    <span style="font-family:'Microsoft YaHei';font-size:14px;line-height:2;">&nbsp;被遗弃的双性患儿小政政</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;2012年，60岁的河南农民樊喜法在海南三亚打工时，在绿化带中发现了一名双性弃婴小政政。此后三年多，樊喜法带着小政政多次往返于海南、河南、北京等地看病，希望通过手术能让小政政过上正常人的生活。可是，高额的医疗费却让生活困难的樊喜法几乎陷入绝境，他不得不四处求助、历尽艰难。幸运的是，樊喜法的大爱义举引起了各界的积极响应，他们纷纷为小政政募集善款、联系医院。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;2016年2月1日，小政政住进北京儿童医院治疗，并被诊断出患有先天性心脏病。3月，小政政接受了先天性心脏病手术。也正是在北京儿童医院做的染色体和内分泌检测显示，小政政的性别倾向于男孩。但要变为男儿之身，小政政必须接受尿道下裂手术。但是，数额庞大的手术费从何而来？专业的医院和医生如何寻找？正当樊喜法再次陷入困境时，新闻媒体和名医主刀向他伸出了援手。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>名医慈善项目在行动</strong></span><br />
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;此前的4月份，国内移动医疗手术平台名医主刀在人民大会堂发起了“共享名医资源•共铸健康中国”公益手术捐助活动，当天便募集了365台公益手术。这也让媒体了解到了名医主刀，并主动联系到了名医主刀的工作人员。出于企业的社会责任感，名医主刀联手爱心公益大使、著名演员王艳积极做出响应，以实际行动在第一时间为樊喜法和小政政“雪中送炭”。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;看着病痛中的小政政，身为人母的王艳感同身受，她带着儿子球球，在名医主刀联合创始人吴勇的陪同下一起去看望了樊喜法父子俩。早前，球球将自己录制节目所得的30万元收入悉数捐给了名医主刀公益项目，用于资助需要手术的贫困儿童。此次，球球决定拿出其中一部分捐赠给小政政，帮助他完成手术。不仅如此，懂事的球球还送给小政政不少玩具，并安慰他不要怕、要勇敢。捐赠现场，许多感人瞬间令人动容，尤其是当王艳和“球球”一起将捐款送到小政政手中时，这个坚强的小男孩接过钱并抱着自己的养父一顿狂亲，直到下楼时还紧紧抓住钱不放，生怕弄丢了，因为他知道那是他的“救命钱”啊。此情此景，让在场的每个人内心久久难以平静。</span>
+                </p>
+                <p>
+                   <img src="http://static.mingyizhudao.com/147010940680285">
+                </p>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;与此同时，名医主刀也马不停蹄，立即联系了北京整形外科医院的知名专家唐勇主任，并迅速安排了手术床位，让小政政顺利地接受了尿道下裂手术。待年底再接受一次尿道下裂手术后，小政政便可完全站着小便。接下来，小政政还将做最后的变性手术，彻底变成一个小男孩。</span><br />
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">从接到媒体的援助请求，到迅速联系知名医生、安排床位到手术成功，名医主刀再次打出了一记漂亮的“组合拳”。分析人士指出，这主要得益于其庞大的专家资源和高效的运营模式优势。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>搭建专家和患者的精准对接平台</strong></span><br />
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;据了解，作为中国最大的移动医疗手术平台，名医主刀自2014年10月上线以来，已汇集3万名三甲医院副主任级别及以上医生，建立了一个强大的专家资源数据库。这些医生主要来源于北上广等中心城市的三甲医院，广泛分布于各大科室，科研实力雄厚，诊疗经验丰富。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;以专家资源库为基础，以“互联网+”为链接，名医主刀在这些知名专家和需要手术的的患者之间搭建了一个精准对接平台。通过分析患者的实际病情，名医主刀会在专家资源库里依据专业领域、空闲时间等因素进行最优匹配，医患双方达成一致意见后，平台再帮助联络空置床位和手术台进行手术。如此一来，普通患者不仅享受到了全国一流专家的手术服务，而且手术等待时间和综合成本也大大缩减，切实享受到了“互联网+”和医疗改革的福利。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;专家指出，在当前我国医疗改革的大潮下，名医主刀的运营模式高度契合了“分级诊疗”和“多点执业”两大主题, 有效盘活了名医专家的业余时间和地方普通医院的闲置床位资源，不仅缓解了百姓看病难、看病贵的问题,专家的专业价值和个人价值也实现了最大化。</span>
+                </p>
+                <p>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><br />
+                    </span>
+                </p>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; &nbsp; &nbsp;大医精诚、大爱无言。汇聚天下名医，为更多患者造福，接下来，名医主刀还将招募更多专家入驻，并引进全球领先的手术技术，为中国的医疗改革和公益事业做出应有的贡献。</span><br />
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"></span><span><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"></span><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"></span></span>
+            </div>
+            <?php $this->renderPartial('//news/pages/_storiesmenu', $data = 'zhitongche'); ?>
+        </div>
+    </div>
+</div>
