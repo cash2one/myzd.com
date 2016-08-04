6109dfdd7385e9c3b9e05f8d7a2fe708bb6e1256
xudongjian <958965736@qq.com>
2016-08-04 11:56:38 +0800

    新专题

---
+++ themes/v5/views/event/pages/tiger.php
@@ -0,0 +1,163 @@
+<?php 
+Yii::app()->clientScript->registerCssFile("http://static.mingyizhudao.com/pc/event-main100.min.css");
+?>
+<style>
+    #site-content{padding-bottom:0!important;}
+</style>
+<section id="tiger">
+    <div class="background-all text-center">
+        <img src="http://static.mingyizhudao.com/147020456091656">
+    </div>
+    <div class="container">
+        <div class="background-color">      
+            <div class="border-one ">
+                <div class="row">
+                    <div class="col-sm-9 text-14 mt30">7月23日下午，北京八达岭野生动物园发生一起老虎伤人的惨剧，1名当事人当场死亡、另有一人受伤，目前还在医院救治中。
+                        悲剧的背后，既有天灾的事由，也有人祸的成分。惨烈的结局也不得不让我们思考：<br><strong>万一某天自己遭遇猛虎，该怎样避免悲剧发生？</strong></div>
+                    <div class="col-sm-3 "><img style="float: right;" src="http://static.mingyizhudao.com/147020374060913"></div>
+                </div>
+            </div>
+            <div class="mt10">
+                <div><img style="margin-left: 50px;" src="http://static.mingyizhudao.com/147021141002259"></div>
+                <div class="border-two">
+                    <div class="line-one">遭遇紧急情况下自救方法：</div>
+                </div>
+            </div>
+            <div class="row mt50 ">
+                <div class="tiger-padding-50">
+                    <div class="col-sm-6 text-center">
+                        <div class="text-16"><strong>防虎第一式：冷静+观察</strong></div>
+                        <div class="border-three">
+                            <div class="mt15">面对老虎，首先，必须得冷静，冷静思考、观察老虎动向，为自己逃生做足准备。慌张奔跑很难逃出老虎的追击。另外，如果是一群人，最好，一群人一起防御会队老虎产生震慑作用。</div>
+                            <div class="mt15">如果老虎离你有些距离，那么抓紧时间看看周边有没有藏身或躲避点，如果有的话，躲到躲避点，这样生存的概率会大很多，起码给自己争取了更多思考和周旋的空间。</div>
+
+                        </div>
+                    </div>
+                    <div class="col-sm-6 text-center">
+                        <div class="text-16"><strong>防虎第二式：且战且退</strong></div>
+                        <div class="border-three">
+                            <div>如果观察到老虎尾巴挺得笔直，说明老虎是直冲你来的。这时候的情形就是，你需要直面老虎了。以下这些东西或许能帮助到你：<div>
+                                    <div class="mt15">1.较大的物件：最好有肉块，没有的话类似如背包、石头、桌椅、木棍等物品都可以，肉块、石头等用于投掷，吸引老虎的注意，其他的诸如背包、木棍等都可以作为防卫的工具；</div>
+                                    <div class="mt15">2.一切能扩大自己体积的东西：如展开外套、举起背包，如果带着小孩，可以将孩子举起超过头顶。老虎一般不会攻击身形高大的动物，所以显得高大也是吓退老虎的一种方式。</div>
+                                    <div class="mt15">在争取到短暂的喘息时机之后，下面就要考虑撤退了。</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 text-center mt30">
+                        <div class="text-16"><strong>防虎第三式：胆大心细</strong></div>
+                        <div class="border-three">
+                            <div>在撤退过程中，千万不要背对老虎！背对老虎很容易让老虎觉得你很容易攻击。撤退时应该要慢慢回退并随时观察老虎的动向，不要太快，适时退到安全区域。<div>
+                                    <div class="mt15">如果撤退失败，老虎再次近身，甚至直扑过来，记住一定不要蹲下来！站稳脚跟，挥舞双手或外套，表示你并不是毫无准备。如果老虎进行攻击，尽量要回应攻击，击打它的面门，包括眼睛等区域。</div>
+                                    <div class="mt15">最后，如果不幸真的遭到老虎的凶猛攻击，一定要记住保护好喉咙和脖子部位。动物习惯从后颈撕咬猎物，保护好颈部有利于争取最大可能的少受伤害、顺利存活下来。</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 text-center mt30">
+                        <div class="text-16"><strong>防虎终极式：名医主刀</strong></div>
+                        <div class="border-three">
+                            <div>当然，如果不幸被老虎咬伤。没事，我们有医生啊。做手术，就找名医主刀。名医主刀，国内最大的手术预约平台。<div>
+                                    <div class="mt15">今天特别推荐以下骨科医生，下载【名医主刀】皆可预约。</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mt50">
+                        <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '2970')); ?>">
+                            <div class="border-four">                           
+                                <div class="text-center"><img class="media-object" src="http://static.mingyizhudao.com/147021522292742" alt="章莹" title="章莹"></div>
+                                <div class="clearfix text-16 ml50">
+                                    <div class="pull-left mt10"><strong>章莹</strong></div>
+                                    <div class="pull-left">
+                                        <div class="text-16 ml50">
+                                            主任医师<br>
+                                            广州军区总医院
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt20">
+                                    <div class="text-16 ml20"><strong>推荐理由</strong></div>
+                                    <ul class="disc">
+                                        <li>科室水平全国领先</li>
+                                        <li>科室主任</li>
+                                        <li>广东省创伤学会副主任委员</li>
+                                        <li>广东省显微外科学会常委</li>
+                                    </ul>
+                                </div>
+                                <div class="mt30">
+                                    <div class="text-16 ml20"><strong>擅长手术</strong></div>
+                                    <div class="text-14 ml20"style="line-height: 20px;">
+                                        骨折，关节损伤，各种骨不连，骨缺损，周围神经损伤诊治，组织修复及矫形手术，
+                                        关节镜技术，AO内固定技术，关节置换技术和显微外科技术。 
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 mt50" >
+                        <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '3236')); ?>">
+                            <div class="border-four">                           
+                                <div class="text-center"><img class="media-object" src="http://static.mingyizhudao.com/147021522208595" alt="李延妮" title="李延妮"></div>                  
+                                <div class="clearfix text-16 ml50">
+                                    <div class="pull-left mt10"><strong>李延妮</strong></div>
+                                    <div class="pull-left">
+                                        <div class="text-16 ml50">
+                                            主任医师<br>
+                                            北京天坛普华医院
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt20">
+                                    <div class="text-16 ml20"><strong>推荐理由</strong></div>
+                                    <ul class="disc">
+                                        <li>天坛普华医院医疗总监</li>
+                                        <li>外科主任</li>
+                                        <li>曾赴美国知名医疗机构执业17年</li>
+                                        <li>获得美国执业医师执照</li>
+                                    </ul>
+                                </div>
+                                <div class="mt30">
+                                    <div class="text-16 ml20"><strong>擅长手术</strong></div>
+                                    <div class="text-14 ml20" style="line-height: 20px;">
+                                        手外科领域（上肢）创伤、神经和肌腱修复、骨关节损伤、小儿先天畸形、游离组织移植修复、骨与软组织缺损及再造乳房。 
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4 mt50 mb50">
+                        <a target="_blank" href="<?php echo $this->createUrl('doctor/view', array('id' => '3137')); ?>">
+                            <div class="border-four">                           
+                                <div class="text-center"><img class="media-object" src="http://static.mingyizhudao.com/147021522107226" alt="孙玉强" title="孙玉强"></div>
+                                <div class="clearfix text-16 ml50">
+                                    <div class="pull-left mt10"><strong>孙玉强</strong></div>
+                                    <div class="pull-left">
+                                        <div class="text-16 ml50">
+                                            主任医师<br>
+                                            上海市第六人民医院
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt20">
+                                    <div class="text-16 ml20"><strong>推荐理由</strong></div>
+                                    <ul class="disc">
+                                        <li> 科室水平全国领先</li>
+                                        <li>科室主任</li>
+                                        <li>中华医学会创伤分会全国青年委员</li>
+                                        <li>成功手术治疗800多例骨盆与髋臼骨折病</li>
+                                    </ul>
+                                </div>
+                                <div class="mt30">
+                                    <div class="text-16 ml20"><strong>擅长手术</strong></div>
+                                    <div class="text-14 ml20" style="line-height: 20px;">
+                                        四肢骨折和脱位，复合型髋臼骨折等骨科疾病的治疗。
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
