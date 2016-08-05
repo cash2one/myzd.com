35ad8c2b2bb76377b9ca3ba5cd48634be18de1ca
by.chang <cby_18@hotmail.com>
2016-08-05 16:40:24 +0800

    no message

--- themes/v5/views/order/viewOne.php
+++ themes/v5/views/order/viewOne.php
@@ -30,8 +30,8 @@ $urlBookingList = $this->createUrl('booking/list');
 $this->show_baidushangqiao = false;
 $refno = $model->getRefNo();
 //$payUrl = $this->createAbsoluteUrl('pay/payment/doPingxxPay');  // url of creating payment request.
-$payUrl = 'http://www.mingyizhudao.com/pay/payment/doPingxxPay';
-
+//$payUrl = 'http://www.mingyizhudao.com/pay/payment/doPingxxPay';
+$payUrl = $this->createAbsoluteUrl('pay/payment/doPingxxPay');
 $refUrl = $this->createAbsoluteUrl('order/view', array('refno' => $refno)); // 
 //$currentUrl = Yii::app()->request->getHostInfo() . Yii::app()->request->getUrl();    // url of this currently requested page.
 
