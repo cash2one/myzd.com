fd0ebcd90550e23b85aac2698e75fadb85c9106b
xudongjian <958965736@qq.com>
2016-08-10 10:55:24 +0800

    患者故事、公司资讯内页

---
+++ themes/v5/views/news/pages/publicdaily.php
@@ -0,0 +1,54 @@
+<?php
+$baseUrlImage = Yii::app()->theme->baseUrl . "/images/";
+?>
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title>a{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="">
+            <div class="col-sm-9 news-content pt30">
+                <div style="text-align:center;">
+                    <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>互联网医疗+纸媒，迈出联姻第一步</strong></span>
+                </div>
+                <p>
+                    <br />
+                </p>
+                <br />
+                <div style="text-indent: 2em;"><span style=" line-height:2;font-size:14px;font-family:'Microsoft YaHei';">7月26日，大众日报与新兴互联网医疗平台“名医主刀”签署战略合作协议，迈出省级党媒与“互联网+医疗”平台的跨界联合的第一步。</span></div>
+                <img src="http://static.mingyizhudao.com/147073097834814">
+                <div style="text-align:center;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">名医主刀创始人兼CEO苏舒、大众日报社广告部总经理于江</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">根据合作协议，双方将秉持充分整合社会资源，全心服务社会大众的初衷，期望打造互联网+时代，主流媒体与互联网新秀强势联姻，资源互补，和谐共赢，传播仁爱的新典范。</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">针对我省当前医疗资源分布不均的现状，以互联网技术为突破口的“互联网+医疗”模式，被看作是重构医疗生态格局的新希望，“名医主刀”就在此时应运而生。</span></div>
+                <img src="http://static.mingyizhudao.com/147073098003992">
+                <div style="text-align:center;"><span style=" line-height:2;font-size:14px;font-family:'Microsoft YaHei';">名医主刀创始人兼CEO苏舒</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">作为目前国内最大的移动手术平台，“名医主刀”专注于患者手术刚需，通过O2O模式，汇集全国上万名三甲医院副主任及以上级别医师，为有手术需求的患者，在第一时间预约专家，安排入院手术，提供专业、高效、安全的手术医疗服务。目前，手术量已达一万多台。此外，“名医主刀”还热心公益，联合全国优秀医务工作者，外科专家组建志愿者联盟，每年定向捐赠手术，以互联网+医疗+公益的新模式，为医疗行业开拓了一套新思路，新方法。</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">山东省作为人口大省、经济强省，全省健康事业发展更具复杂性与紧迫性。山东医改的快速高效稳步推进，已经成为全国新医改非试点省份中的佼佼者，山东健康事业强劲的发展势头，为与名医主刀的战略合作提供了理论和实践探索的沃土。</span></div>
+                <img src="http://static.mingyizhudao.com/147073098118326">
+                <div style="text-align:center;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">签约仪式现场</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">8月10日，由大众报业集团与山东省卫生和计划生育委员会联手推出的“山东省第一届城市公立医院改革高峰论坛”将在省城济南举办。届时，论坛将推进城市公立医院改革作为我省深化医改的关键一步，啃下新医改的最后一块“硬骨头”。此次论坛将以我省全面推开的城市公立医院改革为契机，围绕改革转型期城市公立医院的发展方向和策略，城市公立医院制度改革与管理创新方法等议题进行广泛讨论。国内著名医改专家学者，省市各级卫生工作相关负责人，业界精英将齐聚一堂，一场城市公立医院综合改革的巨型头脑风暴即将来袭。针对本次论坛，大众日报将集合骨干力量，采取多种形式进行全程报道。“名医主刀”将为论坛提供全程服务与强有力的支持。</span></div>
+                <img src="http://static.mingyizhudao.com/147073098143583">
+                <div style="text-align:center;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">名医主刀、大众日报与会人员合影</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">此外，大众日报和“名医主刀”还将在山东医学人文建设、健康山东项目、山东大众健康智库建设等方面展开深度合作，共同助推山东健康事业的又好又快发展。</span></div>
+                <div>
+                    <br />
+                </div>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"></span>
+
+            </div>
+            <?php $this->renderPartial('//news/pages/_newsmenu', $data = 'publicdaily'); ?>
+        </div>
+    </div>
+</div>
---
+++ themes/v5/views/news/pages/repeatcustomers.php
@@ -0,0 +1,56 @@
+
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="">
+            <div class="col-sm-9 news-content pt30">
+                <p>
+                <div style="text-align:center;">
+                    <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>手术还有“回头客”？</strong></span>
+                </div>
+                <p>
+                    <br />
+                </p>
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">回头客，是指在商店、旅馆、饭馆等场所再次光顾的顾客。在生活中，我们很容易就成为某个地点的回头客，可是，做手术还有“回头客”，您是否听说过呢？</span></div><br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">今天名医君要跟大家分享徐女士的故事，看看到底是什么样的服务，能让一个人在众多选择中坚定不移的选择一个平台去匹配手术呢？</span></div>
+                <div class="text-center"><img src="http://static.mingyizhudao.com/147062552041668"></div>
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">徐女士来自美丽富饶、渔产资源丰富的浙江舟山，今年45岁， 没想到健康却跟她开了一个不大不小的玩笑，4月25日徐女士在当地医院检查出左侧肾上腺肿瘤，良性或恶性暂不明确，当地医院只是建议手术治疗，这让以前从没接触过这种大病的徐女士一下慌了神，不知该怎么面对。</span></div>
+                <br />
+                <span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;"><strong>几经转辗，艰难求医</strong></span><br />
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">回想起之前一次看病经历，徐女士突然有了主意。原来发现这次的左肾上腺肿瘤之前，徐女士今年年初，因为妇科疾病面诊过上海仁济医院的妇科专家，当时是通过一个互联网平台预约的，在经历过平台细致的服务后，对平台产生了很高的信任感，与名医主刀的第一次接触让她似乎一下子看到了治好病的曙光。</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">在医疗技术不断提升的今天，很多欠发达地区的医疗水平仍然令人堪忧。徐女士所在当地医院，甚至不能为她确诊病情，更不敢轻举妄动的开刀，尝够了等待和不确定的滋味儿，又回想起年初通过互联网平台联系的上海专家，徐女士做出了决定。</span></div>
+                <br />
+                <span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;"><strong>回头客，源自细致、贴心的服务</strong></span><br />
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">当机立断，徐女士再一次拨通了名医主刀的热线电话，与医疗客服沟通过后，徐女士明确了自己所患疾病系泌尿外科科室，于是便想预约华东地区比较有名的专家为自己确诊并手术。名医主刀汇聚了全球3万名专家以及数千张手术床位资源，医疗客服联系了专门负责签约专家的同事，预约到了华东地区技术较高、声誉较好的上海华山医院泌尿外科主任医师沈周俊教授，沈教授同时也是名医主刀的签约医生，徐女士便放宽了心，准备来上海看病了。</span></div>
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">为了让徐女士更安心的完成就诊过程，名医主刀还特地安排了带她面诊过的医疗客服小冯，小冯曾经在年初带徐女士面诊过仁济医院的妇科专家，这让徐女士倍感贴心。来到华山医院面诊沈教授，完成一系列检查后，徐女士便返回安徽老家等待检查结果，为了省去徐女士来回上海的麻烦，小冯亲自去到医院帮患者取检查报告，并第一时间联系专家给出治疗方案，这时的徐女士只是在老家安静的呆着，并没有感受到三甲医院人满为患的拥堵与烦躁。</span></div>
+                <br />
+                <span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;"><strong>一切为了患者，为了患者的一切</strong></span><br />
+                <br />
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">10天后，徐女士的检查结果出来了，医生判断肿瘤为良性，但仍需手术切除。徐女士悬着的心终于是放下来了，于是出发赶往上海进行手术，令徐女士万万没想到的是，甚至连床位都安排好了，这么贴心的安排，实在让她感到幸福又难以置信。由于肿瘤为良性，所以切除手术进行的很顺利，术后第一天，客服小冯再次来到华山医院探望患者，并留下了这张手术床边亲切温馨的合影。</span></div>
+                
+                <div class="text-center"><img style="width: 500px;height:500px;" src="http://static.mingyizhudao.com/147062552061131"></div>
+                <div style="text-indent: 2em;"><span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;">为了让您体验到最高效便捷的就诊服务，名医主刀愿不辞辛劳，竭尽所能。您的健康，由我们来守护！</span></div>
+                <div>
+                    <br />
+                </div>
+                <span style="font-size:14px;font-family:'Microsoft YaHei';line-height:2;"></span>
+                </p>
+
+            </div>
+            <?php $this->renderPartial('//news/pages/_storiesmenu', $data = 'kangai'); ?>
+        </div>
+    </div>
+</div>
