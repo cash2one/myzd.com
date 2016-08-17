f05943e08f339645ca6c8c3319f2c99d7a1a037e
xudongjian <958965736@qq.com>
2016-08-17 14:26:33 +0800

    名医说手术

---
+++ themes/v5/views/event/pages/doctorLive.php
@@ -0,0 +1,34 @@
+<?php
+Yii::app()->clientScript->registerCssFile("http://static.mingyizhudao.com/pc/event-main101.min.css");
+?>
+<section id="doctorLive">
+    <div class="container-fluid">
+        <div class="row">
+            <img src="http://static.mingyizhudao.com/147133072370139">
+        </div>
+    </div>
+    <div>
+        <div class="container ">
+            <div class="text15 doctorlive-content">
+                <div class="mt50 text-indent2em lineh1-8em">
+                    8月30日，名医主刀将联合健康时报、暖阳基金共同举办“名医说手术——神外专家花椒直播”活动，此次直播由三甲医院神外专家与神秘大咖联手为大家带来“神外手术你应该知道的那些事”和“患者手术诊断书二次确诊”两个环节。作为国内医疗行业与互联网直播平台的首次结合，名医说手术活动旨在借助时下当红的直播平台覆盖更广泛的人群，让医疗健康知识的普及更加生动易懂。   
+                </div>
+                <div class="color-e50011 mt50 text-indent2em">本次直播的内容共针对两类人群：</div>
+                <div class="mt20 point-icon">一是神经外科的手术患者，众所周知，神经外科分支下的脑肿瘤，脑血管及脊髓等疾病的手术一直是高难度的代名词。而对于需要手术的患者来说，手术确诊及手术方案的制定尤为重要。<span class="color-e50011">8月15-25日</span>名医主刀将<span class="color-e50011">招募10名</span>已经确诊需要手术的患者提交手术诊断书，专家将以直播形式对诊断书进行二次确诊。<strong>患者可通过以下方式报名：</strong></div>
+                <div class="row mt20">
+                    <div class="col-sm-6 text-indent2em">
+                        <div class="color-f39700">方法1：扫描以下二维码，进入患者社群。</div>
+                        <div class="text-center mt31"><img src="http://static.mingyizhudao.com/147133142316125"></div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="color-f39700 clearfix">
+                            <div class="pull-left text-indent2em h2em">方法2：</div><div>微信搜索“m18721207747”添加名医小助手，并加入患友社群报名</div>
+                        </div>
+                        <div class="text-center mt10"><img src="http://static.mingyizhudao.com/147133142552599"></div>
+                    </div>
+                </div>
+                <div class="mt50 text-indent2em point-icon">二是有相关病症的患者和潜在人群，专家会针对常见的神外问题进行“神外手术你应该知道的那些事”科普互动，把正确的预防、治疗等相关医疗知识传播出去。</div>
+            </div>
+        </div>
+    </div>
+</section>
