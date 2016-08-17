4cd2ab94a9d3ac46e1f3a00927adfa4ee9102241
xudongjian <958965736@qq.com>
2016-08-17 14:25:15 +0800

    公司资讯内页

---
+++ themes/v5/views/news/pages/deanmanifesto.php
@@ -0,0 +1,72 @@
+<?php
+$baseUrlImage = Yii::app()->theme->baseUrl . "/images/";
+?>
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title>a{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="">
+            <div class="col-sm-9 news-content pt30">
+                <p>
+                <div style="text-align:center;">
+                    <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>郎永淳担纲主持，120家公立医院发表院长宣言</strong></span>
+                </div>
+                <br />
+                <br />
+                <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">8月10日，由山东省卫生和计划生育委员会、大众报业集团(大众日报社)主办、名医主刀承办的山东省第一届城市公立医院改革高峰论坛暨首届院长论坛在济南举行。</span></div>
+                <img src="http://static.mingyizhudao.com/147132961905034">
+                <p>
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">会议由原央视著名主持人郎永淳担纲主持，山东省卫生和计划生育委员会主任袭燕，大众报业集团（大众日报社）党委书记、董事长、总编辑傅绍万为论坛揭幕并致辞。中欧国际工商学院卫生管理与政策中心主任蔡江南，中国人民大学医改研究中心主任王虎峰，温州医科大学附属第一医院原院长陈肖鸣等10位专家成为首批特聘专家。智库专家组组长、中国工程院院士、山东省肿瘤医院院长于金明代表致辞。山东医师协会会长、山东省千佛山医院院长孙洪军宣读院长宣言，名医主刀创始人兼CEO苏舒就“互联网+ 医疗”模式进行了深度解读。同时会议还吸引了各省(部、市)属城市公立医院负责人等300余位专家学者参与，并有120余家公立医院院长联合发表院长宣言。</span></div>
+                    <img src="http://static.mingyizhudao.com/147132962499616">
+                </p>
+                <p>
+                    <img src="http://static.mingyizhudao.com/147132963022742"><br>
+                    <div class="mt20"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>亮点一：郎永淳专业+出彩的主持</strong></span></div>
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">郎永淳出彩的主持，源于朗先生医学专业出身。本次现身山东首届城市公立医院改革高峰论坛，纯属热爱，纯属专业，纯属公益！</span></div>
+                    <img src="http://static.mingyizhudao.com/147132963255254">
+                    <br />
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">此次论坛主题为“改革转型期城市公立医院的发展方向和策略”。多位到场嘉宾就此议题发表了精彩演讲，并剖析了医改中城市公立医院亟待破解的难题和应对措施，全面提高医疗服务质量，充分利用互联网技术，深入贯彻落实国家“分级诊疗+多点执业”政策。</span></div>
+                    <br />
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>亮点二：蔡江南受聘名医主刀首席战略顾问</strong></span><br />
+                    <br />
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">中欧国际工商学院卫生管理与政策中心主任蔡江南教授在会议现场以《如何解决医疗服务的痛点》为议题发表了精彩主题演讲。值得一提的是，蔡教授根据多年对中国医改以及互联网医疗行业的深度研究和理解，高度认可了名医主刀的方向和定位，尤其是对名医主刀在配合国家多点执业、分级诊疗方针政策落地上采取的接地气、脚踏实地的措施给予了高度的赞赏。</span></div>
+                    <br />
+                    <img src="http://static.mingyizhudao.com/147132963521356">
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">此次城市公立医院改革高峰论坛暨首届院长论坛，蔡教授更是全程参与了策划、组织和安排，为会议提供了诸多有建设性的建议和专业的指导。通过此次会议的合作，加深了对名医主刀的认知和认可。会议期间，蔡教授欣然接受邀请，担任名医主刀首席战略顾问。蔡教授更坦言名医主刀未来有潜力成为最大的互联网医生集团，从而更好的将体制内和体制外的医生凝聚在一起，更高效的实现医生的价值，为患者提供及时、便捷的服务。</span></div>
+                    <br />
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>亮点三：苏舒的五点独树一帜</strong></span><br />
+                    <br />
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">名医主刀创始人兼CEO苏舒对眼下大热的“互联网+医疗”模式从医疗资源分配的角度进行解读，发表了《“互联网+”如何助力医疗资源效率》的主题演讲。</span></div>
+                    <br />
+                    <img src="http://static.mingyizhudao.com/147132964014077">
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">作为目前国内领先的移动医疗手术平台，名医主刀经过将近2年的发展，已拥有了超过3万名外科专家资源以及数千张手术床位资源，平台每月平均手术量突破6000台，已成为名副其实的“互联网+手术”基地。</span></div>
+                    <br />
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">苏舒在演讲中提到，互联网平台对推动国家医改政策的实施有着天然的优势，信息对称、精准匹配、解决病痛、品牌塑造、医疗保险以上五点都是名医主刀在众多移动医疗品牌中独树一帜的有力抓手。苏舒还介绍了名医主刀的金牌项目“互联网手术中心”、“名医公益”、“名医铸刀”等，赢得了在场院长们的一致认可和赞赏。</span></div>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp;</span><br />
+                    <img src="http://static.mingyizhudao.com/147132964325783">
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">作为为数不多的坐落在上海的移动医疗公司，名医主刀将和同样坐落在上海的中欧商学院，尤其是蔡江南教授领衔的医改政策中心进行更紧密的合作，并将在今年的奇璞奖大会上共襄盛举。</span></div>
+                    <br />
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>亮点四：120家公立医院联合发表院长宣言</strong></span><br />
+                    <br />
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">宣言号召公立医院保持公益属性，全面提高医疗服务质量，创造城市公立医院发展的山东模式，为健康山东建设作出贡献。</span></div>
+                    <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp;</span><br />
+                    <img src="http://static.mingyizhudao.com/147132964676635">
+                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">作为此次论坛的承办方，名医主刀将在山东这片土地上深挖精耕，会同平台3万多名外科专家，为健康山东的事业作出独特的具有互联网+医疗特色的贡献！</span></div>
+                <div>
+                    <br />
+                </div>
+                <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"></span>
+                </p>
+
+            </div>
+            <?php $this->renderPartial('//news/pages/_newsmenu', $data = 'deanmanifesto'); ?>
+        </div>
+    </div>
+</div>
