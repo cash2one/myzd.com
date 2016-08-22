4b5bd77e53ff170fae8490b301d10a418d45ae75
lijuntaosh <466900988@q.com>
2016-08-22 17:52:31 +0800

    疾病详情页404

--- protected/controllers/DiseaseController.php
+++ protected/controllers/DiseaseController.php
@@ -5,6 +5,9 @@ class DiseaseController extends WebsiteController {
     public function actionView($id) {
         $apiService = new ApiViewDisease($id);
         $data = $apiService->loadApiViewData();
+        if (count($data)<=1) {
+            $this->throwPageNotFoundException();
+        }
         $this->render('view', array(
             'data' => $data
         ));
