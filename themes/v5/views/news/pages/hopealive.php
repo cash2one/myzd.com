9bb5d17f260ed169370b0560356074968884ef98
xudongjian <958965736@qq.com>
2016-08-25 14:51:56 +0800

    患者故事

---
+++ themes/v5/views/news/pages/hopealive.php
@@ -0,0 +1,58 @@
+<?php
+$urlVideo = Yii::app()->theme->baseUrl . "/videos/";
+?>
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="col-sm-9 news-content pt10">
+            <p>
+            <div style="text-align:center;">
+                <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>他说，活下去的希望是你们给的！</strong></span>
+            </div>
+            <p>
+                <br />
+            </p>
+            <br />
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">2016年7月，我们在安徽宿州一家医院里看到了这对可怜的老夫妻。躺在病床上的老人名叫郑安详，今年60岁。但他的晚年并不像他的名字那样安详。</span></div>
+            <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">视频</span><br />
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">60岁，本该是安享晚年的年纪，但命运弄人。本就一贫如洗的老人，偏偏却祸不单行，一场疾病彻底打垮了这对流浪的老夫妻。</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191864724680"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">“我经常哭，经常掉眼泪，心里过的不愉快。”郑安详妻子说。“什么活都不能干，饭都不能弄，还要端给他吃。我也一身都是病。”</span></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">这究竟是一个怎样的家庭？病魔又是如何折磨他们？这对老两口将会迎来怎样的命运？</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191865198954"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">郑安详到了医院门诊以后，通过询问病史，医生才知道他家庭比较困难，老两口靠常年在外收破烂维持生活。老有所依，这是每一位老人最基本的需求，但现实却恨恨地甩了他们一巴掌。</span></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">四年前，当时身体健朗的郑安详被突如其来的一场病疾病打倒了。由于生活条件的拮据，郑安详一直采用保守治疗，身体状态一天不如一天。老郑走路开始一瘸一拐，出现了肌肉萎缩的症状，走路都成了问题。</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191865385749"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">“没享过福，一想掉眼泪，我们也没有过上好日子，怎么办呢。”郑安详妻子说道。</span></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">为了帮助这对老两口重拾生活的信心，社会各界动员了起来。来自上海的程光齐教授受名医主刀邀请赶到了宿州，为老人进行一次公益手术，帮他们渡过难关。</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191865535834"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">在病房里，程教授对老人进行了一次检查。“这个病人术前我也检查了一下，是一个左侧的股骨头坏死，然后他的行走很痛，他下肢的患侧要比右侧短两公分左右。这个左面髋关节活动的疼痛是很厉害的，主要是左面的股骨头坏死，病程已经有四五年了。”程教授介绍了老郑的基本病情。</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191865942549"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">在当地医院、手术器材商、名医主刀，以及程教授的努力下，很快，一场公益手术开始了。</span></div>
+            <div class="text-center"><img style="height:280px;width:480px;" src="http://static.mingyizhudao.com/147191866108437"></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">可喜的是，郑安详的病情得到了控制，他或许不能像过去那样下地干活，不能像正常人一样正常下蹲，但社会各界的帮助让这对老两口看到了生活的希望，但愿他们可以渡过一个安详的晚年，未来的路一帆风顺。</span></div>
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">对于这次从上海来到县级医院进行公益手术，程教授感触良多，也非常欣慰。他说：“我觉得这次的公益手术包括以后的合作，最大的得益者是病人是患者。如果病人要到一个大的医院，人生地不熟，床位也很紧张，找专家也不一定能够找到。那么现在通过名医主刀这样一个平台，搭起来一个桥梁，能联系大医院有经验的医生和地方医院的合作和交流，然后分享一些先进的经验和技术，更好的为病人服务，我觉得这是最大的收获。”</span></div>
+            <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp;</span><br />
+            <div class="text-center">
+                <embed src="http://static.video.qq.com/TPout.swf?vid=v0321s2efwt&auto=0" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+            </div>
+            <div>
+                <br />
+            </div>
+            </p>
+            <p>
+                <br />
+            </p>
+
+        </div>
+        <?php $this->renderPartial('//news/pages/_storiesmenu', $data = 'mingyizhudao'); ?>   
+    </div>
+</div>
