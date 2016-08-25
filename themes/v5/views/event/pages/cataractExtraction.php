c79063ff0a4ffc5d536d020da8f980608b2a93ff
xudongjian <958965736@qq.com>
2016-08-25 15:05:30 +0800

    白内障复明公益

---
+++ themes/v5/views/event/pages/cataractExtraction.php
@@ -0,0 +1,40 @@
+<?php
+Yii::app()->clientScript->registerCssFile("http://static.mingyizhudao.com/pc/event-main102.min.css");
+?>
+<style>#site-content{padding-bottom:0!important;}</style>
+<section id="cataractExtraction">
+    <div class="contaier-fluid">
+        <div class="row">
+            <div class="container">
+                <div style="background-color:#f7f7f7;line-height:1.8em;">
+                    <div class="cataractExtraction">
+                        <div class="text18 text-center">
+                            <div class="text21"><strong>让爱带来光明</strong></div>
+                            <div class="text16 mt10">白内障复明公益项目正式开启啦！</div>
+                        </div>
+                        <div class="first-content">
+                            <div class="mt50">白内障复明是一项康复工程、民心工程，为了将这项惠民工程办实办好，此次活动由名医主刀发起，江宁区残疾人联合会、南京市老年大学、南京医科大学附属逸夫医院、小芳健康网等组织共同举办，为白内障患者实施复明公益项目，希望能为他们带去光明。</div>
+                            <div class="mt50">共筛查<strong class="color-e50011">200名患者</strong>（在医院就诊过为基础），其中40例由南京逸夫医院完成手术，<strong class="color-e50011">前10例</strong>符合要求的患者（贫困、军烈属优先）将获得北京专家的全公益手术，晶体由上海兴靓医疗器械有限公司倾情赞助。</div>
+                        </div>
+                        <div class="text-center">
+                            <div class="color-555">
+                                <div class="text18 mt50"><span>报名时间</span></div>
+                                <div class="text16 mt10">2016年8月24日——2016年8月28日，招满即止</div>
+                                <div class="text18 mt50"><span>筛查时间</span></div>
+                                <div class="text16 mt10">8月29日、30日两天上午8:30—11:00</div>
+                                <div class="text16">地址：南京市江宁区龙眠大道109号南京医科大学附属逸夫医院</div>
+                                <div class="text18 mt50"><span>申请条件</span></div>
+                                <div class="text16 mt10">4-75岁之间，此前未接受过白内障手术，且无其他并发疾病的贫困患者可申请此公益手术，待名医主刀医疗团队审核告知结果。</div>
+                                <div class="text18 mt50"><span>申请流程</span></div>
+                                <div class="text16 mt10">①关注名医主刀微信公众号<strong>（myzd02）</strong>，回复<strong>“白内障”</strong>后按提示进行操作。</div>
+                                <div class="text16">②拨打名医主刀热线电话<strong class="color-19aea5">400-6277-120</strong>，报名白内障公益手术。</div>
+                                <div class="mt50 text16">活动期间有任何疑问，可以微信后台留言或拨打名医主刀热线<strong class="color-19aea5">400-6277-120</strong>，我们会在第一时间答复您。</div>
+                            </div>
+                            <div class="mt10 text12 color-e50011 pb100">注：本次10例白内障全公益手术，只包含手术相关的费用，来往路费不包含在内。</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
