8f9c19f639558b75fbdfef0dfa57da1a8abed9a5
lijuntaosh <466900988@q.com>
2016-09-01 16:47:39 +0800

    搜索

--- protected/models/disease/DiseaseSearch.php
+++ protected/models/disease/DiseaseSearch.php
@@ -16,7 +16,7 @@ class DiseaseSearch extends ESearchModel {
 
     public function addQueryConditions() {
         $this->criteria->addCondition('t.date_deleted is NULL');
-        $this->criteria->addCondition('t.app_version = 7');
+        $this->criteria->addCondition('t.app_version = 8');
 
         if ($this->hasQueryParams()) {
             // Disease.Name
