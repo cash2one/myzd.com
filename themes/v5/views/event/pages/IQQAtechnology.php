6ea4f00ec071729b75f9ff680bf165eb14bccf73
xudongjian <958965736@qq.com>
2016-09-07 16:02:01 +0800

    IQQA专题首页

---
+++ themes/v5/views/event/pages/IQQAtechnology.php
@@ -0,0 +1,148 @@
+<?php
+Yii::app()->clientScript->registerCssFile("http://static.mingyizhudao.com/pc/event-IQQAtechnology.min.css");
+?>
+<section class="background-color" id="IQQA-technology">
+    <div class="banner-bg text-center">
+        <img src="http://static.mingyizhudao.com/147312958892497">
+    </div>
+    <div class="container mt50">
+        <div class="IQQA-technology-padding">
+            <div class="text-14">
+                2016 年名医主刀的延安“红色公益之旅”活动联合 EDDA 科技， 为当地一名肝癌患者实施手术。为了能在术前给患者制订一个最佳的手
+                术方案，主刀医生李志伟教授和延安当地医生一起使用了上海 EDDA 科技为全国各地外科医生开放的IQQA技术服务，在术前为患者进行精
+                准的手术模拟切除三维设计，手术专家们对使用 IQQA 精准手术规划技术平台所得到的“精”和“准”的临床辅助给予了高度评。
+            </div>
+            <div class="mt30 text-24 text-center">
+                看得见、说得清、道得明，主刀医生还可以无血模拟手术？
+            </div>
+            <div class="mt15 text-14">
+                造桥、造路都要事先仔细设计规划，动手术更应如此。IQQA精准手术规划技术服务定位于病人病症确诊后和治疗前之间，服务病人、服务
+                医生、架接病人与医生间桥梁。IQQA技术服务有效应用于各种癌症（尤其是肝、胆、胰、脾、胃、肺、肾等肿瘤）的术前精准模拟规划、
+                术后定量评估及随访。
+            </div>
+            <div class="mt15">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>病人的数字化脏器和病灶、虚拟手术模拟</strong>
+                </div>
+            </div>
+            <div class="mt15 ml30">
+                <div class="text-14-20">
+                    <img class="mr20 pull-left" src="http://static.mingyizhudao.com/147313319394642">IQQA技术服务可清晰、直观的显示并定量脏器内各组织的解剖结构，精准定位出
+                    病灶的位置及病灶与周围血管的空间毗邻关系；通过交互式的三维模型实施虚拟手术，临床医生可根据手术需要任意调整分割面的位置、角度、曲度等，
+                    进而有效进行手术方案的优化，提升手术的安全性，降低手术过程中可能出现的潜在风险，使患者获得最佳的治疗效果。
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="mt30 text-24 text-center">
+                筑专业品牌，为健康护航：IQQA技术服务能为肿瘤治疗带来哪些帮助?
+            </div>
+            <div class="mt15">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>IQQA技术服务适应于哪些手术？</strong>
+                </div>
+            </div>
+            <div class="mt15 text-14 ml30">
+                已获得美国FDA、欧盟CE、中国CFDA及台湾DOH等多方国际认证的IQQA精准手术规划技术平台，涵盖重大癌症（尤其是胸、腹、盆腔肿瘤）治疗的术前精准模拟规划、术后定量评估及随访。
+            </div>
+            <div class="text-14">
+                <img class="mr10" src="http://static.mingyizhudao.com/147314259691672"><strong>肝胆胰：</strong>肝脏肿瘤精准外科切除、肝移植、肝门部胆管癌、胆道肿瘤、胰腺肿瘤、肝肿瘤介入消融治疗等方面的术前精准评估和规划等
+            </div>
+            <div class="text-14">
+                <img class="mr10" src="http://static.mingyizhudao.com/147314259691672"><strong>肺：</strong>精准的肺段切除手术，肺叶内肿瘤介入消融手术等方面的术前精准评估和规划等
+            </div>
+            <div class="text-14">
+                <img class="mr10" src="http://static.mingyizhudao.com/147314259691672"><strong>肾：</strong>部分肾切除规划、肾上腺肿瘤切除评估、经皮肾镜穿刺清石手术、肾肿瘤消融计划和评估等
+            </div>
+            <div class="mt15">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>IQQA技术服务流程</strong>
+                </div>
+            </div>
+            <div class="text-center border-process mt15">
+                <img src="http://static.mingyizhudao.com/147314344735916">
+            </div>
+            <div class="mt10">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>IQQA技术服务优势</strong>——实时、交互、快捷、全量化
+                </div>
+            </div>
+            <div class="mt15 text-14">
+                <div><img class="mr10" src="http://static.mingyizhudao.com/147314259691672">在肝脏三维影像应用中，精准度、智能化、自动化水平最高；</div>
+                <div><img class="mr10" src="http://static.mingyizhudao.com/147314259691672">智能2D/3D信息融合及高级3D显示，支持用户实时校验及调整；</div>
+                <div><img class="mr10" src="http://static.mingyizhudao.com/147314259691672">支持医生在对肝脏/肝段、肿瘤及肝脏内部管道复杂的解剖结构做出快捷、直观、个体化、全量化的即时定位定量分析；</div>
+                <div><img class="mr10" src="http://static.mingyizhudao.com/147314259691672">为医生提供全面、快捷客观定量分析与术前术后评估的临床解决方案，辅助医生进行更精准的术前模拟规划和术后评估。</div>
+            </div>
+            <div class="mt15">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>众多荣誉，凸显卓越品质</strong>
+                </div>
+            </div>
+            <div class="text-center border-process mt15 pb25">
+                <img src="http://static.mingyizhudao.com/147314508989491">
+            </div>
+            <div class="mt30">
+                <div class="text-18 ">
+                    <img class="mr10" src="http://static.mingyizhudao.com/147313319388984"><strong>“大城市的专家医生就在我们的‘IQQA云服务平台’上”</strong>
+                </div>
+            </div>
+            <div class="text-14-20" style="text-indent:2em">
+                <img class="ml15 pull-right" src="http://static.mingyizhudao.com/147314509260318">       
+                <div>2016年，EDDA科技将自主创新研发的，已获得CFDA和FDA的批准的IQQA-3D肿瘤精准手术产品系列，推广运用到了IQQA网络云服务平台上，
+                从而使更多基层医院的医生和癌症患者获益于这一成熟、安全、有效的高科技技术服务。</div>
+                <div>在对接好的IQQA云服务平台上，异地专家可以在同一平台上对同一个三维全量化的病例进行异地同步互动，实时参与治疗方案制定、交互
+                修改、模拟手术，从而大大提升了异地专家向病人当地的主治医生提供临床咨询、指导、或赴实地帮助手术的效率。同时也节约了老百姓
+                时间和金钱，有效实现在家门口就可以看到全国名医的愿望，并能为增进医患沟通及改进医患关系提供看得见、说得清、道得明的帮助。</div>
+            </div>
+            <div class="clearfix"></div>
+            <div class="mt30 text-24 text-center">
+                IQQA技术服务在精准医疗上的经典成功案例
+            </div>
+            <div class="row mt30">
+                <div class="col-lg-6">
+                    <div class="border-four">
+                        <img class="ml2" src="http://static.mingyizhudao.com/147314670896784">
+                        <div class="mt10  text-14 "><strong>“除瘤保肾”大战告捷！ IQQA-3D术前精准评估助北京大学第一医院成功实施罕见肾脏巨大肿瘤切除术：</strong>患者青年女性，经行
+                        增强CT诊断为左肾巨大错构瘤，肿瘤大小约22*13*33cm，体积巨大，十分罕见；手术团队在IQQA术前精准规划辅助下制定了周
+                        密手术方案；历时4小时的手术奋战，成功摘除巨瘤，顺利完成保留肾功能的肾部分切除手术……</div>
+                        <a href="http://service.eddatech.com.cn/dynamic_twosix.html" target="_blank"><div class="detailed">点击查看详情></div></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="border-four">
+                        <img class="ml2" src="http://static.mingyizhudao.com/147314671195338">
+                        <div class="mt10  text-14 ">
+                            <strong>全国首例母子“拼肝”手术 - 精准外科成就心肝宝贝的健康新生，上海市第一人民医院运用IQQA精准手术规划技术和快速建模实例：</strong>来自贵州
+                            的10岁男孩志豪，因肝功能存在严重缺陷，需做肝移植治疗。手术团队经过慎密思考，决定进行"拼肝"手术：即切除患儿半边肝脏，将剩余部分
+                            与妈妈捐献的半边肝拼接起来，以保证供受双方都能保有所需的正常肝功能，手术顺利实施……</div>
+                        <a href="http://service.eddatech.com.cn/dynamic_six.html" target="_blank"><div class="detailed">点击查看详情></div></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt30">
+                    <div class="border-four">
+                        <img class="ml2" src="http://static.mingyizhudao.com/147314674864779">
+                        <div class="mt10  text-14">
+                            <strong>IQQA技术云服务成功连接名医主刀延安红色公益救助：</strong>不分天南海北，无论地角天涯，多地、实时、交互、全定量的IQQA精准手术规划云服务平
+                            台就在你的医院、就在你主刀医生使用的指尖.......在2016年5月的某一时刻，北京—延安—上海聚焦成一点：从北京飞延安的主刀专家、革命
+                            老区延安当地手术中心的病友、在上海的IQQA精准手术规划平台云服务......
+                        </div>
+                        <a href="http://service.eddatech.com.cn/dynamic_twofour.html" target="_blank"><div class="detailed">点击查看详情></div><a>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt30">
+                    <div class="border-four">
+                        <img class="ml2" src="http://static.mingyizhudao.com/147314677266675">
+                        <div class="mt10  text-14">
+                            <strong>【速度+精度】IQQA精准手术云服务助重症胆囊癌患者重获新生：</strong>年近60的病友，因中上腹隐痛2月，皮肤巩膜黄染1月，CT检查显示胆囊癌侵犯肝
+                            门部、肝固有和右肝动脉、门静脉右支，临床诊断为：胆囊癌侵犯肝门。肿瘤生长的位置已侵犯到了肝门部、肝动脉、门静脉等重要血管，技术分
+                            析难度很高，上海交通大学医学院附属仁济医院胆胰外科的王坚教授......
+                        </div>
+                        <a href="http://service.eddatech.com.cn/dynamic_tensix.html" target="_blank"><div class="detailed">点击查看详情></div></a>
+                    </div>
+                </div>
+            </div>
+            <div class="mt30 mb100 text-14-color">
+                如果您对IQQA技术感兴趣，可以直接拨打客服热线400-6277-120，我们专业的医疗客服人员将给您提供专业的意见和建议。
+            </div>
+        </div>
+    </div>
+</section>
