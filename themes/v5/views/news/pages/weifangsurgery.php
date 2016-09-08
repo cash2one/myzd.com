0919855b1a1554571c23f8a9b4542aeef355eac5
xudongjian <958965736@qq.com>
2016-09-08 14:53:50 +0800

    新专题

---
+++ themes/v5/views/news/pages/weifangsurgery.php
@@ -0,0 +1,91 @@
+<?php
+$urlVideo = Yii::app()->theme->baseUrl . "/videos/";
+?>
+<style>
+    .news-content{border: 1px solid #e4e4e4;font-size: 16px;padding-left: 30px;padding-right: 30px;}
+    .newslist{padding: 20px 10px;}
+    .newslist .news-title{font-size: 24px;color: #19aea5;}
+    .newslist .news-title .new-slogan{font-size: 12px; color: #9f9fa0;margin-left: 10px;padding-left: 10px;border-left: 1px solid #9f9fa0;}
+    .newslist .news-item{margin-bottom: 10px;}
+    .newslist ol li{overflow: hidden;height: 1.5em;}
+    .newslist ol li .fa-star{color: #EFA429;font-size: 12px;}
+</style>
+<div class="container">
+    <div class="mt10">
+        <div class="col-sm-9 news-content pt10">
+            <p>
+            <div style="text-align:center;">
+                <span style="font-family:'Microsoft YaHei';font-size:18px;line-height:2;"><strong>名医公益行，潍坊爱心手术！</strong></span>
+            </div>
+            <p>
+                <br />
+            </p>
+            <br />
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">8月18日下午，潍坊市人民医院手术室，一台特殊的爱心手术正在有序进行。</span></div>
+            <br />
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">由来自山东省立医院的国内知名小儿心外科专家王安彪教授主刀，为一名患先天性心脏病的小患者进行手术。</span></div>
+            <br />
+            <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>女患儿因费用受困家中</strong></span><br />
+            <br />
+            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">“小时候她的身体就虚弱，三岁的时候开始感冒发烧。”说起11岁的小患者青青（化名）的病情，母亲不禁抹起了眼泪。当时家人只以为是正常的感冒，一直给小青青打针、吃药，直到五岁那年发现平卧呼吸困难、乏力月余，也没法跟小朋友一起参加活动才意识到问题的严重性，随到潍坊市人民医院检查，才被确诊为先心病房间隔缺损。</span></div>
+            </p>
+            <p>
+                <span><span style="font-size:14px;line-height:28px;"><br />
+                        <img src="http://static.mingyizhudao.com/147280266716283">
+                        <div class="text-center"><span style="line-height:2;font-size:12px;font-family:'Microsoft YaHei';">青青的主治医生孙忠东主任到病房看望</span></div>
+                        </p>
+                        <p>
+                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">据青青主治医生潍坊市人民医院心外科副主任孙忠东介绍，先天性先心病房间隔缺损是外科一种比较常见的畸形，发现后及时手术，可以恢复正常人水平，如果延误病情，则会导致寿命减短。</span></div><br>
+                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">“医生告诉我们能够手术治疗，可手术费在四五万左右，到哪儿去弄这么多钱？”听到医生的诊断，青青一家喜忧参半。青青的父亲常年在外打工每月工资也就两千元，母亲在家种地，同时还要照顾年迈的老母亲及青青和年幼弟弟，整个家庭的年收入才两万多块钱。因无法拿出突如其来的巨额医疗费，青青父母决定先回家，攒几年钱再为她治疗。</span></div><br>
+                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">时间如流水，一晃六年过去了，青青的嘴唇开始绀紫，病情越来越严重，父母亲再次带着青青来到潍坊市人民医院，而且因为年纪的增大，手术难度和手术费也比当初增加不少。青青的父母亲把身边能借到钱的亲人、朋友都借了但还不够，面对如此高昂的手术费，青青的父母亲又一次陷入了绝望，决定暂缓手术，再次带青青返回家中等待。</span></div><br>
+                        </p>  
+                                    <img src="http://static.mingyizhudao.com/147280266909382">
+                                    <div class="text-center"><span style="line-height:2;font-size:12px;font-family:'Microsoft YaHei';">提到青青的病情，家人还是难忍泪水</span></div>
+                                    </p>
+                                    <p>
+                                        <br />
+                                        <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>名医公益伸出援助之手&nbsp;</strong></span><br />
+                                        <br />
+                                        <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">正当一家人愁眉不展时，孙忠东主任医师带来了好消息，他帮青青联系到了“名医主刀”“百万公益基金援助计划”，并能够邀请到我国知名小儿心外科专家、山东省立医院王安彪教授来主刀手术。</span></div><br>
+                                    </p>
+                                    <p>
+                                        <span><span style="font-size:14px;line-height:28px;"><br />
+                                                <img src="http://static.mingyizhudao.com/147280267096344">
+                                                <div class="text-center"><span style="line-height:2;font-size:12px;font-family:'Microsoft YaHei';">名医主刀工作人员与王安彪教授（左三）合影</span></div>
+                                                </p>
+                                                <p>
+                                                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">能够请到王安彪这样的专家，对青青的病情无疑将起到非常大的帮助。王安彪专家专攻于先天性心脏病、风湿性心脏瓣膜病和冠心病及大血管疾病的诊治。尤其是对婴幼儿先天性心脏病诊治有着丰富的临床经验和深入的研究，精于体外循环及心肌保护技术，完成过多项心脏外科高难度手术，是山东省立医院心外科婴幼儿复杂先心手术的重要主刀医师，多项手术技术处于国内、省内领先水平。</span></div><br>
+                                                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">&nbsp; &nbsp; 8月18日下午3点30分，青青的父母亲及舅舅早已等待在手术室外面。面对即将开始的手术，年幼的青青显得有些紧张，亲人们不断的给她加油：“别害怕，手术结束就没事了，加油......”目送着青青被缓缓推进手术室。接下来，家人们一直守候在手术室门外，一步也不敢离开。</span></div><br>
+                                                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">经过2小时的救治，青青的手术完美结束。“手术很成功，和潍坊本地孙忠东副主任配合的也比较好。”术后，王安彪专家第一时间告诉担忧的家属。考虑到女孩未来的生长发育和美观需要，还专门为她设计了刀口的位置：先天性先心病房间隔缺损正常开膛手术会留下很大疤痕，对于小女孩来说可能会有心理负担；但开右胸是比较美容的。只是由于年龄较大，体重35公斤与成人相仿，对手术增加了难度。</span></div><br>
+                                                    <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">好在专家经验丰富，手术的结果还是比较完美的，术后一般只需要七天的恢复期，恢复后寿命跟正常人一样，也不会影响正常发育。</span></div><br>
+                                                </p>
+                                                <p>
+                                                            <img src="http://static.mingyizhudao.com/147280267350014">
+                                                            <div class="text-center"><span style="line-height:2;font-size:12px;font-family:'Microsoft YaHei';">乐观的青青，面对疾病依然面带甜甜的笑容</span></div>
+                                                            <br />
+                                                            <span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';"><strong>多方联动成就爱心手术</strong></span><br />
+                                                            <br />
+                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">潍坊市人民医院心外科在吴海军主任的带领下，目前科室有专业团队共26人，9名大夫，17名护士，年均手术量300台左右，位于省内前列。自1984年第一例心脏搭桥手术至今已有32年的临床经验。</span></div><br>
+                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">这次，在“名医主刀”的帮助下，能请到国内知名专家，更加提振了整个团队的信心。“名医主刀”团队了解到这一情况，马上帮忙联系到山东省立医院小儿心外科医师王安彪教授，让家长和医护人员心里有了底。</span></div><br>
+                                                            </p>
+                                                            <p>
+                                                                        <img src="http://static.mingyizhudao.com/147280266598755">
+                                                                        <div class="text-center"><span style="line-height:2;font-size:12px;font-family:'Microsoft YaHei';">手术中的王安彪教授</span></div>
+                                                                        </p>
+                                                                        <p>
+                                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">据了解，这也是王安彪通过互联网平台实施的第一例公益手术。据“名医主刀”山东区域负责人王培明介绍，得知到患者情况时，小青青一家人已因为费用问题回到老家，“名医主刀”团队迅速开展实地考察核实家庭状况，并决定为手术提供尽可能的帮助，保证青青能在最短的时间内获得救治。</span></div><br>
+                                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">“没有您们有就没有俺孩子的明天！”了解到女儿手术后，很快就可以恢复如常，青青的妈妈激动地留下了眼泪不停地向王安彪、孙忠东和名义主刀团队表示衷心感谢！</span></div><br>
+                                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">青青的病情此次能得到有效的治疗，正是互联网+医疗的一次成功案例。据了解，“名医主刀”创立于2014年，该平台通过打造医疗手术O2O，用互联网+的思路构建新的服务，优化医疗资源的利用效率，打造开放平台及服务生态。</span></div><br>
+                                                                            <div style="text-indent: 2em;"><span style="line-height:2;font-size:14px;font-family:'Microsoft YaHei';">目前，“名医主刀”手术平台已汇集了全国3万余名三甲医院副主任级别及以上的医生，月手术量已经达到近万台。此外，对于因家庭经济因素确需要救助的患者，“名医主刀”还将通过慈善公益组织捐赠手术费用，积极协助对接优秀医师，到有爱心的医院实施公益手术。</span></div><br>
+                                                                        <div>
+                                                                            <br />
+                                                                        </div>
+                                                                        </p>
+                                                                        <p>
+                                                                            <br />
+                                                                        </p>
+
+                                                                        </div>
+                                                                        <?php $this->renderPartial('//news/pages/_storiesmenu', $data = 'mingyizhudao'); ?>   
+                                                                        </div>
+                                                                        </div>
