582905e97325002d87d8587ba1bfd2026d53aa11
xudongjian <958965736@qq.com>
2016-09-29 11:06:14 +0800

    三星体检中心专题

---
+++ themes/v5/views/event/pages/samsung.php
@@ -0,0 +1,196 @@
+<?php 
+Yii::app()->clientScript->registerCssFile("http://static.mingyizhudao.com/pc/event-samsung.min.css");
+?>
+<div class="container-fluid search-hospital-header" style="background-color:#19AEA5;height:45px;">
+    <div class="container">
+        <div class="crumbs-header">
+            <ul>
+                <li><a href="<?php echo Yii::app()->homeUrl; ?>">首页</a></li>
+                <li>></li>
+                <li><a href="<?php echo $this->createUrl('event/view/eventList'); ?>">专题列表</a></li>
+                <li>></li>
+                <li class="main-department">详情</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<section id="samsung">
+    <div class="bg-banner text-center">
+        <img src="http://static.mingyizhudao.com/147495792816470">
+    </div>
+    <div class="samsung-color">
+        <div class="container">
+            <div class="samsung-padding">
+                <div class="mt50 biaoqian text-21 pl30">
+                    江北三星医院综合体检中心
+                </div>
+                <div class="mt20 text-18">
+                    <strong>最早和最佳</strong>——延续韩国最早的声誉，发展成为世界最佳体检中心
+                </div>
+                <div class="mt20 text-15">
+                    1968年开院的江北三星医院作为三星集团的一员，以技术及服务革新为基础，五十多年来，为医疗技术的发展做出了贡献。
+                </div>
+                <div class="mt20 text-15">
+                    1981年，最早将综合体检概念引入韩国，各领域专业医疗团队通过尖端医疗设备，提供最佳健康管理服务。江北三星医院综合体检中心作为韩国最早、最大的体检中心，实现着预防疾病及国民的健康管理，不断开启医学的未来。
+                </div>
+                <div class="vedio-area text-center mt20">
+                    <video id="my-video" class="vedio-content" src="http://static.mingyizhudao.com/pc/samsungHospital.mp4"  data-setup="{}" type="video/mp4" controls></video>
+                </div>
+                <div class="mt5 color-7970b1 text-center">江北三星医院综合体检中心宣传片</div>
+                <div class="mt40 text-16">
+                    <span class="pr10"><img class="mb3" src="http://static.mingyizhudao.com/147496099819862"></span>韩国最早、最大的体检中心
+                </div>
+                <div class="row mt20">
+                    <div class="col-md-4">
+                        <img src="http://static.mingyizhudao.com/147496229598035">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="border">
+                            <div class="text-15">
+                                <p style="text-indent:0;"><strong>亮点一  是韩国最早、最大的体检中心，具备优秀的技术和服务实力。</strong></p>
+                                <p class="mt15">依靠三星集团的技术及服务实力，提供最上乘的医疗服务。</p>
+                                <p class="mt15">江北三星医院综合体检中心作为价值得到世界公认的三星集团的一员，共享着集团的实力。</p>
+                                <p class="mt15 pb8">1981年，最早将综合体检概念引入韩国，开展体检服务。目前，具备有韩国最大规模的设施，保有最多的体检顾客。今后，将依靠持续开展医疗技术及服务革新，不断积累顾客的信赖。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt40 text-16">
+                    <span class="pr10"><img class="mb3" src="http://static.mingyizhudao.com/147496099819862"></span>最尖端医疗设备
+                </div>
+                <div class="row mt20">
+                    <div class="col-md-4">
+                        <img src="http://static.mingyizhudao.com/147496230275749">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="border">
+                            <div class="text-15">
+                                <p style="text-indent:0;"><strong>亮点二  依靠最尖端医疗设备和先进医疗系统，实现快速、精确的体检。</strong></p>
+                                <p class="mt30">使用最尖端医疗设备，更快、更准确地进行体检，为顾客的健康负责。</p>
+                                <p class="mt30 pb16">江北三星医院综合体检中心引进癌症诊断设备——PET-CT，对肿瘤、脑部疾病及心脏疾病等进行更精密的检查，还具备脑肿瘤、脑梗死、脑动脉瘤、脑血管狭窄等的诊断率较高的MRI；提供腹部脏器的立体影像；提高检查精确性的MDCT；可对多种脏器的构造、移动及血流等进行精密检查的超声波设备等。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt40 text-16">
+                    <span class="pr10"><img class="mb3" src="http://static.mingyizhudao.com/147496099819862"></span>各领域最佳专业医疗团队一站式诊疗
+                </div>
+                <div class="row mt20">
+                    <div class="col-md-4">
+                        <img src="http://static.mingyizhudao.com/14749623087026">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="border">
+                            <div class="text-15">
+                                <p style="text-indent:0;"><strong>亮点三  拥有各领域最佳专业医疗团队，一站式诊疗</strong></p>
+                                <p class="mt15">实力优秀的专业医疗团队提高体检的质量。</p>
+                                <p class="mt15">江北三星医院综合体检中心综合管理各分科具有十年以上丰富经验的专业医疗团队。由此，通过精确的诊断和细致的咨询，致力于早期发现疾病。</p>
+                                <p class="mt15 pb24">同时，由医生、护士、营养师和临床心理医生提供综合健康咨询。综合体检中心还与江北三星医院合作，从体检后分析到治疗，提供一站式诊疗服务。</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt40 text-16">
+                    <span class="pr10"><img class="mb3" src="http://static.mingyizhudao.com/147496099819862"></span>与约翰霍普金斯大学签订谅解备忘录
+                </div>
+                <div class="row mt20">
+                    <div class="col-md-4">
+                        <img src="http://static.mingyizhudao.com/147496231381297">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="border">
+                            <div class="text-15">
+                                <p style="text-indent:0;"><strong>亮点四&nbsp;&nbsp;  成为世界第一研究机构&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;约翰霍普金斯大学在亚洲的唯一的合作伙伴，向着世界综合体检中心的目标不断飞跃。</strong></p>
+                                <p class="mt15">通过与世界级研究机构开展合作，提供最佳医疗服务。</p>
+                                <p class="mt15 ">江北三星医院综合体检中心作为世界第一研究机构——约翰霍普金斯大学选择的健康体检领域唯一的合作伙伴，问诊、检查、诊断、结果、评价等所有体检系统都具备了全球标准化，提供体系化的体检服务。此外，与海外学者一起举行研讨会等，地位获得了世界认可。江北三星医院综合体检中心与世界一起开展研究，立志提供不断发展的服务。 </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt50 biaoqian text-21 pl30">
+                    三星体检项目产品套餐
+                </div>
+                <div class="border-two mt15">
+                    <div class="border-three">
+                        <div class="border-four border-text-18">
+                            <a data-id="package1" class="active"><strong>套餐一&nbsp;&nbsp;面体检项目</strong></a>
+                        </div>
+                        <div class="border-four border-text-18">
+                            <a data-id="package2"> <strong>套餐二&nbsp;&nbsp;癌症精密体检项目</strong></a>
+                        </div>
+                        <div class="border-four border-text-18">
+                            <a data-id="package3"><strong>套餐三&nbsp;&nbsp;两天一夜深度体检项目</strong></a>
+                        </div>
+                    </div>
+                    <div class="package active" id="package1">
+                        <p class="text-16-color"><strong>套餐一&nbsp;&nbsp;面体检项目</strong></p>
+                        <div class="text-14">
+                            <p>1. 价格：¥22,500</p>
+                            <p>2. 时间：行程三天两夜，一天体检。</p>
+                            <p>3. 体检地点：江北三星医院首尔综合体检中心</p>
+                            <p>4. 体检项目<span style="color:#87ccc5"><strong>（基础项目 ＋ 附加项目）</strong>:</span></p>
+                            <p><span style="color:#87ccc5"><strong>- 基础项目</strong></span></p>
+                            <p>咨询国际诊疗专家, 身体测量及肥胖度(身高, 体重, 体重指数, 腰围, 肌肉量, 体内脂肪量, 体内脂肪比例, 腹部脂肪量), 肺功能检查, 听力检查, 胸部X光检查, 血液检查(包括贫血, 肾功能, 肝功能, 免疫功能, 肿瘤指标, 心脑血管, 糖尿病, 甲状腺等共67种血液检查), 尿液及粪便检查, 眼科检查(视力, 眼压, 眼底检查), 心电图及血压检测, 胃镜或胃造影, 腹部超声(肝, 胆, 胰, 脾, 肾), 睡眠(疗法), 代谢综合症, 医学生物年龄, 综合诊断(专家诊断), 营养咨询, 光盘刻录。</p>
+                            <p><span style="color:#87ccc5"><strong>- 附加项目</strong></span></p>
+                            <p>•男性 : 高半胱氨酸, 动脉硬化检查(PWV)</p>
+                            <p>•女性(未婚) : 甲状腺超声, 风疹, 乳房X光检查</p>
+                            <p>•女性(已婚) : 甲状腺超声, 妇科检查, 人乳头状瘤病毒(HPV), 乳房X光检查</p>
+                            <p>5. 配套服务：</p>
+                            <p>签证，往返机票(出发城市北京，上海，大连)，专车服务（接送机，往返医院，首尔旅游），4星级酒店（两夜），翻译（医疗翻译＋旅游翻译），首尔旅游（N首尔塔，景福宫，明洞，包括餐费和门票费）。</p>
+                            <p>6. 行程安排：</p>
+                            <img src="http://static.mingyizhudao.com/147505400259695">
+                        </div>
+                    </div>
+                    <div class="package" id="package2">
+                        <p class="text-16-color"><strong>套餐二&nbsp;&nbsp;癌症精密体检项目</strong></p>
+                        <div>
+                            <p>1. 价格：¥39,500</p>
+                            <p>2. 时间：行程三天两夜，一天体检。</p>
+                            <p>3. 体检地点：江北三星医院首尔综合体检中心</p>
+                            <p>4. 体检项目：</p>
+                            <p><span style="color:#87ccc5"><strong>男</strong></span></p>
+                            <p><strong>套餐一项目</strong> + 骨密度(腰椎+大腿部), 低剂量肺部多排螺旋CT, 经颅多普勒超声(TCD), 睡眠结肠镜检查, 心脏超声, 前列腺超声, 甲状腺超声，PET-CT 或全身 MRI。</p>
+                            <p><span style="color:#87ccc5"><strong>女</strong></span></p>
+                            <p><strong>套餐一项目</strong> + 骨密度(腰椎+大腿部), 低剂量肺部多排螺旋CT, 经颅多普勒超声(TCD), 睡眠结肠镜检查, 心脏超声, 乳房超声, 盆腔超声，PET-CT 或全身 MRI。</p>
+                            <p>5. 配套服务：</p>
+                            <p>签证，往返机票(出发城市北京，上海，大连)，专车服务（接送机，往返医院，首尔旅游），4星级酒店（两夜），翻译（医疗翻译＋旅游翻译），首尔旅游（N首尔塔，景福宫，明洞，包括餐费和门票费）。</p>
+                            <p>6. 行程安排：</p>
+                            <img src="http://static.mingyizhudao.com/147505400841137">
+                        </div>
+                    </div>
+                    <div class="package" id="package3">
+                        <p class="text-16-color"><strong>套餐三&nbsp;&nbsp; 深度体检项目</strong></p>
+                        <div>
+                            <p>1. 价格：¥79,500</p>
+                            <p>2. 时间：行程四天三夜，两天体检。</p>
+                            <p>3. 体检地点：江北三星医院水原综合体检中心</p>
+                            <p><strong>套餐二项目 </strong>+ 颅脑 MRI, 颅脑 MRA, 高半胱氨酸, 运动负荷检查, 冠状动脉MDCT, 腰椎X光及腰椎 MRI, 颈椎X光及颈椎 MRI。</p>
+                            <p>5. 配套服务：</p>
+                            <p>签证，<strong>头等舱／公务舱</strong>往返机票(出发城市北京，上海，大连)，<strong>VIP接机服务</strong>（飞机门口接机，VIP通道移动），<strong>EQUUS雅科仕</strong>豪华轿车服务（接送机，往返医院，首尔旅游），<strong>5星级</strong>酒店（三夜），翻译（医疗翻译＋旅游翻译），首尔旅游（N首尔塔，景福宫，明洞，包括餐费和门票费）。</p>
+                            <p>6. 行程安排：</p>
+                            <img src="http://static.mingyizhudao.com/14750540130216">
+                        </div>
+                    </div>
+                </div>
+                <div class="mt50 biaoqian text-21 pl30">
+                    申请方式
+                </div>
+                <div class="text-16 mt10 pl30 mb100"style="color:#555;">
+                    拨打名医主刀热线电话<span style="color:#87ccc5"><strong>400-6277-120</strong></span>，参加三星体验项目。
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".border-four a").click(function () {
+            $(".border-four a").removeClass("active");
+            $(this).addClass("active");
+            var dataId = $(this).attr("data-id");
+            $("#samsung .package").removeClass("active");
+            $("#" + dataId).addClass("active");
+        });
+    });
+</script>
