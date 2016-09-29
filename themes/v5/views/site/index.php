7ab2d233c5750ebb908d9b776627316de69d88c7
xudongjian <958965736@qq.com>
2016-09-29 11:05:16 +0800

    首页轮播图

--- themes/v5/views/site/index.php
+++ themes/v5/views/site/index.php
@@ -236,7 +236,8 @@ $urlBaseUrl = Yii::app()->params['baseUrl'];
 </style>
 <script>
     $(document).ready(function () {
-        $html = '<li><div class="shareEconomic"><a href="<?php echo $this->createUrl('event/view', array('page' => 'shareEconomic')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/147419072489754" alt="" class="img-responsive"></a></div></li>' +
+        $html = '<li><div class="samsung"><a href="<?php echo $this->createUrl('event/view', array('page' => 'samsung')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/147505521586480" alt="" class="img-responsive"></a></div></li>' +
+                '<li><div class="shareEconomic"><a href="<?php echo $this->createUrl('event/view', array('page' => 'shareEconomic')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/147419072489754" alt="" class="img-responsive"></a></div></li>' +
                 '<li><div class="loseweight"><a href="<?php echo $this->createUrl('event/view', array('page' => 'loseweight')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/147505151627490" alt="" class="img-responsive"></a></div></li>' +
                 '<li><div class="loseWeightSurgery"><a href="<?php echo $this->createUrl('event/view', array('page' => 'loseWeightSurgery')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/14733163619513" alt="" class="img-responsive"></a></div></li>' +
                 '<li><div class="IQQAtechnology"><a href="<?php echo $this->createUrl('event/view', array('page' => 'IQQAtechnology')); ?>" target="_blank"><img src="http://static.mingyizhudao.com/147321875452189" alt="" class="img-responsive"></a></div></li>' +
